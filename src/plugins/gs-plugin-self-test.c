07e59d0573087d00bc3dbf08a300ba4a1bc9ed2a
Richard Hughes <richard@hughsie.com>
2013-09-02 14:55:30 +0100

    Add a self-test plugin only to be used in the self-test framework
    This means we can return all applications witouth fully refining them.

---
+++ src/plugins/gs-plugin-self-test.c
@@ -0,0 +1,64 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2011-2013 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+
+#include <gs-plugin.h>
+
+/**
+ * gs_plugin_get_name:
+ */
+const gchar *
+gs_plugin_get_name (void)
+{
+	return "self-test";
+}
+
+/**
+ * gs_plugin_get_priority:
+ */
+gdouble
+gs_plugin_get_priority (GsPlugin *plugin)
+{
+	return 1.0f;
+}
+
+/**
+ * gs_plugin_refine:
+ */
+gboolean
+gs_plugin_refine (GsPlugin *plugin,
+		  GList *list,
+		  GCancellable *cancellable,
+		  GError **error)
+{
+	GsApp *app;
+	GList *l;
+
+	for (l = list; l != NULL; l = l->next) {
+		app = GS_APP (l->data);
+		if (gs_app_get_state (app) == GS_APP_STATE_UNKNOWN) {
+			gs_app_set_state (app, GS_APP_STATE_INSTALLED);
+			gs_app_set_kind (app, GS_APP_KIND_NORMAL);
+		}
+	}
+	return TRUE;
+}
