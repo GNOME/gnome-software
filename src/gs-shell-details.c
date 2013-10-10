29cfb1f77537a7756096b0ced930d12ec1deaf6a
Richard Hughes <richard@hughsie.com>
2013-10-10 10:01:13 +0100

    trivial: Relax the licence length requirement a little

--- src/gs-shell-details.c
+++ src/gs-shell-details.c
@@ -363,7 +363,7 @@ gs_shell_details_refresh_all (GsShellDetails *shell_details)
 		/* TRANSLATORS: this is where the licence is not known */
 		gtk_label_set_label (GTK_LABEL (widget), _("Unknown"));
 		gtk_widget_set_tooltip_text (widget, NULL);
-	} else if (strlen (tmp) > 20) {
+	} else if (strlen (tmp) > 40) {
 		/* TRANSLATORS: this is where the licence is insanely
 		 * complicated and the full string is put into the tooltip */
 		gtk_label_set_label (GTK_LABEL (widget), _("Complicated!"));
