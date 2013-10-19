c436ab50c2624897fa304b6a840c04ddf3e74879
Richard Hughes <richard@hughsie.com>
2013-10-19 20:23:16 +0100

    trivial: Add a sync version of gs_plugin_loader_search()

--- src/gs-plugin-loader-sync.c
+++ src/gs-plugin-loader-sync.c
@@ -78,6 +78,53 @@ gs_plugin_loader_get_installed (GsPluginLoader *plugin_loader,
 }
 
 static void
+gs_plugin_loader_search_finish_sync (GsPluginLoader *plugin_loader,
+				     GAsyncResult *res,
+				     GsPluginLoaderHelper *helper)
+{
+	helper->list = gs_plugin_loader_search_finish (plugin_loader,
+						       res,
+						       helper->error);
+	g_main_loop_quit (helper->loop);
+}
+
+/**
+ * gs_plugin_loader_search:
+ **/
+GList *
+gs_plugin_loader_search (GsPluginLoader *plugin_loader,
+			 const gchar *value,
+			 GsPluginRefineFlags flags,
+			 GCancellable *cancellable,
+			 GError **error)
+{
+	GsPluginLoaderHelper helper;
+
+	/* create temp object */
+	helper.context = g_main_context_new ();
+	helper.loop = g_main_loop_new (helper.context, FALSE);
+	helper.error = error;
+
+	g_main_context_push_thread_default (helper.context);
+
+	/* run async method */
+	gs_plugin_loader_search_async (plugin_loader,
+				       value,
+				       flags,
+				       cancellable,
+				       (GAsyncReadyCallback) gs_plugin_loader_search_finish_sync,
+				       &helper);
+	g_main_loop_run (helper.loop);
+
+	g_main_context_pop_thread_default (helper.context);
+
+	g_main_loop_unref (helper.loop);
+	g_main_context_unref (helper.context);
+
+	return helper.list;
+}
+
+static void
 gs_plugin_loader_get_updates_finish_sync (GsPluginLoader *plugin_loader,
 					  GAsyncResult *res,
 					  GsPluginLoaderHelper *helper)
--- src/gs-plugin-loader-sync.h
+++ src/gs-plugin-loader-sync.h
@@ -32,6 +32,11 @@ GList		*gs_plugin_loader_get_installed		(GsPluginLoader	*plugin_loader,
 							 GsPluginRefineFlags flags,
 							 GCancellable	*cancellable,
 							 GError		**error);
+GList		*gs_plugin_loader_search		(GsPluginLoader	*plugin_loader,
+							 const gchar	*value,
+							 GsPluginRefineFlags flags,
+							 GCancellable	*cancellable,
+							 GError		**error);
 GList		*gs_plugin_loader_get_updates		(GsPluginLoader	*plugin_loader,
 							 GsPluginRefineFlags flags,
 							 GCancellable	*cancellable,
