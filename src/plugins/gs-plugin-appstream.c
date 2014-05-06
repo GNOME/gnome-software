9d496ec2c36384549cb548d9fba3e8266529d292
Richard Hughes <richard@hughsie.com>
2014-05-06 13:08:11 +0100

    Depend on a new appstream-glib

--- src/plugins/gs-plugin-appstream.c
+++ src/plugins/gs-plugin-appstream.c
@@ -45,25 +45,6 @@ gs_plugin_get_name (void)
 }
 
 /**
- * as_app_has_category:
- */
-static gboolean
-as_app_has_category (AsApp *app, const gchar *category)
-{
-	GPtrArray *categories;
-	const gchar *tmp;
-	guint i;
-
-	categories = as_app_get_categories (app);
-	for (i = 0; i < categories->len; i++) {
-		tmp = g_ptr_array_index (categories, i);
-		if (g_strcmp0 (tmp, category) == 0)
-			return TRUE;
-	}
-	return FALSE;
-}
-
-/**
  * as_app_has_compulsory_for_desktop:
  */
 static gboolean
