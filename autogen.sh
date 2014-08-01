45933567f0c05a0e40f85cc9d3851a8ffbef204b
Kalev Lember <kalevlember@gmail.com>
2014-08-01 07:52:39 +0200

    Drop old-style documentation support macros

--- autogen.sh
+++ autogen.sh
@@ -23,4 +23,4 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.7 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.7 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . gnome-autogen.sh
