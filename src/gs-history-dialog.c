8e1ed2d35701c074da6ed3dcdd3788b15d499965
Kalev Lember <kalevlember@gmail.com>
2015-06-16 22:07:59 +0200

    Make rows in history dialog non-activatable
    Since we don't do anything when you click on a row there, they should
    not give the impression that they are activatable.

--- src/gs-history-dialog.c
+++ src/gs-history-dialog.c
@@ -61,6 +61,7 @@ gs_history_dialog_set_app (GsHistoryDialog *dialog, GsApp *app)
 	GDateTime *datetime;
 	GPtrArray *history;
 	GtkBox *box;
+	GtkWidget *row;
 	GtkWidget *widget;
 	guint64 timestamp;
 	guint i;
@@ -145,6 +146,9 @@ gs_history_dialog_set_app (GsHistoryDialog *dialog, GsApp *app)
 
 		gtk_widget_show_all (GTK_WIDGET (box));
 		gtk_list_box_insert (GTK_LIST_BOX (priv->list_box), GTK_WIDGET (box), -1);
+
+		row = gtk_widget_get_parent (GTK_WIDGET (box));
+		gtk_list_box_row_set_activatable (GTK_LIST_BOX_ROW (row), FALSE);
 	}
 }
 
