c561ecb7c2cc5dc20bcae61e81a06eed9accb615
Kalev Lember <klember@redhat.com>
2015-09-07 17:47:45 +0200

    trivial: search provider: Fix indentation

--- src/gs-shell-search-provider.h
+++ src/gs-shell-search-provider.h
@@ -30,12 +30,12 @@
 
 G_DECLARE_FINAL_TYPE (GsShellSearchProvider, gs_shell_search_provider, GS, SHELL_SEARCH_PROVIDER, GObject)
 
-gboolean gs_shell_search_provider_register (GsShellSearchProvider *self,
-                                            GDBusConnection       *connection,
-                                            GError               **error);
-void gs_shell_search_provider_unregister (GsShellSearchProvider *self);
-GsShellSearchProvider * gs_shell_search_provider_new (void);
-void gs_shell_search_provider_setup (GsShellSearchProvider *provider,
-				     GsPluginLoader *loader);
+gboolean		 gs_shell_search_provider_register	(GsShellSearchProvider	 *self,
+								 GDBusConnection	 *connection,
+								 GError			**error);
+void			 gs_shell_search_provider_unregister	(GsShellSearchProvider	 *self);
+GsShellSearchProvider	*gs_shell_search_provider_new		(void);
+void			 gs_shell_search_provider_setup		(GsShellSearchProvider	 *provider,
+								 GsPluginLoader		 *loader);
 
 #endif /* __GS_SHELL_SEARCH_PROVIDER_H */
