8cec1f28ffdccb26d31cc294613fa8dbc2557e5a
Richard Hughes <richard@hughsie.com>
2016-02-17 16:27:22 +0000

    Drop the unsightly '(Nightly)' prefix on xdg-app bundles

--- src/plugins/gs-plugin-appstream.c
+++ src/plugins/gs-plugin-appstream.c
@@ -567,6 +567,8 @@ gs_plugin_refine_item (GsPlugin *plugin, GsApp *app, AsApp *item, GError **error
 	/* set name */
 	tmp = as_app_get_name (item, NULL);
 	if (tmp != NULL) {
+		if (g_str_has_prefix (tmp, "(Nightly) "))
+			tmp += 10;
 		gs_app_set_name (app, GS_APP_QUALITY_HIGHEST, tmp);
 	}
 
