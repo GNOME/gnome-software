853d65aa643c36e18399bd400a07dde5f4d81a5d
Robert Ancell <robert.ancell@canonical.com>
2016-03-02 21:14:32 +1300

    ubuntu-reviews: Depend on appstream to set source information

--- src/plugins/gs-plugin-ubuntu-reviews.c
+++ src/plugins/gs-plugin-ubuntu-reviews.c
@@ -80,7 +80,9 @@ gs_plugin_initialize (GsPlugin *plugin)
 const gchar **
 gs_plugin_order_after (GsPlugin *plugin)
 {
-	static const gchar *deps[] = { NULL };
+	static const gchar *deps[] = {
+		"appstream",
+		NULL };
 	return deps;
 }
 
