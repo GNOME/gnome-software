acbc105d5ca2f3ffe6534e9dc5555de582a55761
Richard Hughes <richard@hughsie.com>
2016-03-23 11:00:38 +0000

    Allow popular and featured apps to match any prefix
    This allows us to specify the short-form 'foobar.desktop' which would be
    matched against the system 'foobar.desktop' AND the prefixed version,
    for instance 'xdg-app-user:foobar.desktop'.

--- src/gs-app.h
+++ src/gs-app.h
@@ -73,6 +73,10 @@ typedef enum {
 
 #define	GS_APP_KUDOS_WEIGHT_TO_PERCENTAGE(w)	(w * 20)
 
+#if !AS_CHECK_VERSION(0,5,12)
+#define AS_APP_QUIRK_MATCH_ANY_PREFIX		(1 << 3)
+#endif
+
 GQuark		 gs_app_error_quark		(void);
 
 GsApp		*gs_app_new			(const gchar	*id);
