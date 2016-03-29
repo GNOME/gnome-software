96aa51db90fa49684290e18f5bd398a2056f84ab
Richard Hughes <richard@hughsie.com>
2016-03-29 20:17:49 +0100

    Fix live installing firmware
    Use an explicit private symbol so the linker doesn't get confused.

--- src/plugins/gs-plugin-fwupd.c
+++ src/plugins/gs-plugin-fwupd.c
@@ -907,15 +907,13 @@ gs_plugin_offline_update (GsPlugin *plugin,
 }
 
 /**
- * gs_plugin_app_install:
- *
- * Called when a user double clicks on a .cab file
+ * gs_plugin_fwupd_install:
  */
-gboolean
-gs_plugin_app_install (GsPlugin *plugin,
-		       GsApp *app,
-		       GCancellable *cancellable,
-		       GError **error)
+static gboolean
+gs_plugin_fwupd_install (GsPlugin *plugin,
+			 GsApp *app,
+			 GCancellable *cancellable,
+			 GError **error)
 {
 	const gchar *filename;
 	gboolean offline = TRUE;
@@ -944,6 +942,20 @@ gs_plugin_app_install (GsPlugin *plugin,
 }
 
 /**
+ * gs_plugin_app_install:
+ *
+ * Called when a user double clicks on a .cab file
+ */
+gboolean
+gs_plugin_app_install (GsPlugin *plugin,
+		       GsApp *app,
+		       GCancellable *cancellable,
+		       GError **error)
+{
+	return gs_plugin_fwupd_install (plugin, app, cancellable, error);
+}
+
+/**
  * gs_plugin_app_update:
  *
  * This is only called when updating device firmware live.
@@ -954,7 +966,7 @@ gs_plugin_app_update (GsPlugin *plugin,
 		      GCancellable *cancellable,
 		      GError **error)
 {
-	return gs_plugin_app_install (plugin, app, cancellable, error);
+	return gs_plugin_fwupd_install (plugin, app, cancellable, error);
 }
 
 /**
