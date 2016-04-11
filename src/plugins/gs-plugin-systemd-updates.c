2a566c967da0cfd39ca3c864d5f742ce575bebf2
Richard Hughes <richard@hughsie.com>
2016-04-11 08:45:57 +0100

    trivial: Set the update state of offline updates correctly

--- src/plugins/gs-plugin-systemd-updates.c
+++ src/plugins/gs-plugin-systemd-updates.c
@@ -143,7 +143,7 @@ gs_plugin_add_updates (GsPlugin *plugin,
 		split = pk_package_id_split (package_ids[i]);
 		gs_app_add_source (app, split[PK_PACKAGE_ID_NAME]);
 		gs_app_set_update_version (app, split[PK_PACKAGE_ID_VERSION]);
-		gs_app_set_state (app, AS_APP_STATE_AVAILABLE);
+		gs_app_set_state (app, AS_APP_STATE_UPDATABLE);
 		gs_app_set_kind (app, AS_APP_KIND_GENERIC);
 		gs_plugin_add_app (list, app);
 	}
