7ac7d2682fbff4fde82a4d7ea13e1d2163d5047e
Richard Hughes <richard@hughsie.com>
2016-04-15 10:27:23 +0100

    Set the management plugin for distribution upgrades
    This allows us to run the correct action plugin when using the self tests.

--- src/plugins/gs-plugin-dummy.c
+++ src/plugins/gs-plugin-dummy.c
@@ -74,7 +74,7 @@ gs_plugin_add_updates (GsPlugin *plugin,
 	gs_app_set_name (app, GS_APP_QUALITY_NORMAL, "Boxes");
 	gs_app_set_summary (app, GS_APP_QUALITY_NORMAL, "Do not segfault when using newer versons of libvirt.");
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
-	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
+	gs_app_set_management_plugin (app, plugin->name);
 	gs_plugin_add_app (list, app);
 	g_object_unref (app);
 
@@ -82,8 +82,8 @@ gs_plugin_add_updates (GsPlugin *plugin,
 	app = gs_app_new ("libvirt-glib-devel;0.0.1;noarch;fedora");
 	gs_app_set_name (app, GS_APP_QUALITY_NORMAL, "libvirt-glib-devel");
 	gs_app_set_summary (app, GS_APP_QUALITY_NORMAL, "Fix several memory leaks.");
-	gs_app_set_kind (app, AS_APP_KIND_GENERIC);
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
+	gs_app_set_management_plugin (app, plugin->name);
 	gs_plugin_add_app (list, app);
 	g_object_unref (app);
 
@@ -91,8 +91,8 @@ gs_plugin_add_updates (GsPlugin *plugin,
 	app = gs_app_new ("gnome-boxes-libs;0.0.1;i386;updates-testing");
 	gs_app_set_name (app, GS_APP_QUALITY_NORMAL, "gnome-boxes-libs");
 	gs_app_set_summary (app, GS_APP_QUALITY_NORMAL, "Do not segfault when using newer versons of libvirt.");
-	gs_app_set_kind (app, AS_APP_KIND_GENERIC);
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
+	gs_app_set_management_plugin (app, plugin->name);
 	gs_plugin_add_app (list, app);
 	g_object_unref (app);
 
@@ -113,8 +113,8 @@ gs_plugin_add_installed (GsPlugin *plugin,
 	gs_app_set_summary (app, GS_APP_QUALITY_NORMAL, "Power Management Program");
 	gs_app_set_state (app, AS_APP_STATE_AVAILABLE);
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
+	gs_app_set_management_plugin (app, plugin->name);
 	gs_plugin_add_app (list, app);
-	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
 
 	return TRUE;
 }
@@ -134,6 +134,7 @@ gs_plugin_add_popular (GsPlugin *plugin,
 	gs_app_set_summary (app, GS_APP_QUALITY_NORMAL, "Power Management Program");
 	gs_app_set_state (app, AS_APP_STATE_AVAILABLE);
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
+	gs_app_set_management_plugin (app, plugin->name);
 	gs_plugin_add_app (list, app);
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
 
@@ -232,6 +233,7 @@ gs_plugin_add_category_apps (GsPlugin *plugin,
 	gs_app_set_state (app, AS_APP_STATE_AVAILABLE);
 	gs_app_set_pixbuf (app, gdk_pixbuf_new_from_file ("/usr/share/icons/hicolor/48x48/apps/gnome-boxes.png", NULL));
 	gs_app_set_kind (app, AS_APP_KIND_DESKTOP);
+	gs_app_set_management_plugin (app, plugin->name);
 	gs_plugin_add_app (list, app);
 	return TRUE;
 }
