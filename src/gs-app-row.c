1b39e8adb00c35048b24dc5cd35e5fd157d7c1df
Richard Hughes <richard@hughsie.com>
2016-07-14 15:31:45 +0100

    Use a smaller star rating widget in the search results

--- src/gs-app-row.c
+++ src/gs-app-row.c
@@ -692,6 +692,7 @@ gs_app_row_init (GsAppRow *app_row)
 
 	gtk_widget_set_has_window (GTK_WIDGET (app_row), FALSE);
 	gtk_widget_init_template (GTK_WIDGET (app_row));
+	gs_star_widget_set_icon_size (GS_STAR_WIDGET (priv->star), 12);
 
 	priv->colorful = TRUE;
 	priv->settings = g_settings_new ("org.gnome.software");
