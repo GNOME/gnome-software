1de80a8f22d4eeb0ec04bc5d7e95ae28ed5c1d09
Robert Ancell <robert.ancell@canonical.com>
2016-10-05 12:14:23 +1300

    Fix use of freed memory.
    The change in 1350bcf fixed the containers (and possibly some of the contents)
    being leaked, but didn't then copy the contents into their new containers.
    
    Noted by @sourcejedi on github.com.

--- src/gs-folders.c
+++ src/gs-folders.c
@@ -199,18 +199,18 @@ load (GsFolders *folders)
 
 		excluded_apps = g_settings_get_strv (settings, "excluded-apps");
 		for (j = 0; excluded_apps[j]; j++) {
-			g_hash_table_add (folder->excluded_apps, excluded_apps[j]);
+			g_hash_table_add (folder->excluded_apps, g_strdup (excluded_apps[j]));
 		}
 
 		apps = g_settings_get_strv (settings, "apps");
 		for (j = 0; apps[j]; j++) {
 			if (!g_hash_table_contains (folder->excluded_apps, apps[j]))
-				g_hash_table_add (folder->apps, apps[j]);
+				g_hash_table_add (folder->apps, g_strdup (apps[j]));
 		}
 
 		categories = g_settings_get_strv (settings, "categories");
 		for (j = 0; categories[j]; j++) {
-			g_hash_table_add (folder->categories, categories[j]);
+			g_hash_table_add (folder->categories, g_strdup (categories[j]));
 		}
 
 		g_hash_table_insert (folders->folders, (gpointer)folder->id, folder);
