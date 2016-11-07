6bc5243f961a15d93a47640c75edd7fa8d44d074
Robert Ancell <robert.ancell@canonical.com>
2016-11-08 09:58:05 +1300

    Hide Kudo details since we don't have good data

--- src/gs-details-page.c
+++ src/gs-details-page.c
@@ -1074,9 +1074,10 @@ gs_details_page_refresh_all (GsDetailsPage *self)
 
 	/* hide the kudo details for non-desktop software */
 	switch (gs_app_get_kind (self->app)) {
-	case AS_APP_KIND_DESKTOP:
+	// Hidden on Ubuntu since don't have appropriate information
+	/*case AS_APP_KIND_DESKTOP:
 		gtk_widget_set_visible (self->grid_details_kudo, TRUE);
-		break;
+		break;*/
 	default:
 		gtk_widget_set_visible (self->grid_details_kudo, FALSE);
 		break;
