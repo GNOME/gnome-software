75e16f694d54305f76415661c8023dce4753fc7e
Richard Hughes <richard@hughsie.com>
2017-02-17 11:09:22 +0000

    trivial: Allow building the flatpak self test data again

--- data/tests/flatpak/build-flatpak.sh
+++ data/tests/flatpak/build-flatpak.sh
@@ -3,11 +3,11 @@ rm -rf repo
 # build test application
 rm chiron/export -rf
 flatpak build-finish chiron
-flatpak build-export repo chiron
 appstream-compose --origin=flatpak --basename=org.test.Chiron --prefix=chiron/files --output-dir=chiron/files/share/app-info/xmls org.test.Chiron
 flatpak build-export repo chiron --update-appstream
 
 # build test runtime
 rm org.test.Runtime/export -rf
 flatpak build-finish org.test.Runtime
-flatpak build-export repo org.test.Runtime --runtime
+appstream-compose --origin=flatpak --basename=org.test.Runtime --prefix=org.test.Runtime/files --output-dir=org.test.Runtime/files/share/app-info/xmls org.test.Runtime
+flatpak build-export repo org.test.Runtime --runtime --update-appstream
