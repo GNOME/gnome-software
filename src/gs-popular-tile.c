d389e075da49ed5ee0831726ce4fff66583c1b00
Richard Hughes <richard@hughsie.com>
2017-05-08 11:05:01 +0100

    trivial: Update more tile widgets if the app name or summary changes

--- src/gs-popular-tile.c
+++ src/gs-popular-tile.c
@@ -118,6 +118,10 @@ gs_popular_tile_set_app (GsAppTile *app_tile, GsApp *app)
 
 	g_signal_connect (tile->app, "notify::state",
 		 	  G_CALLBACK (app_state_changed), tile);
+	g_signal_connect (tile->app, "notify::name",
+			  G_CALLBACK (app_state_changed), tile);
+	g_signal_connect (tile->app, "notify::summary",
+			  G_CALLBACK (app_state_changed), tile);
 	app_state_changed (tile->app, NULL, tile);
 
 	/* perhaps set custom css */
