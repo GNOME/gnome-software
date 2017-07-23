5791ad3a55f6a8dc7a2be43772c4ee2eae3d94fc
Robert Ancell <robert.ancell@canonical.com>
2017-07-24 11:38:04 +1200

    snap: Run before hardcoded-popular plugin
    This stops the hardcoded values from being used.

--- plugins/snap/gs-plugin-snap.c
+++ plugins/snap/gs-plugin-snap.c
@@ -57,6 +57,9 @@ gs_plugin_initialize (GsPlugin *plugin)
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_BETTER_THAN, "packagekit");
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_BEFORE, "icons");
 
+	/* Override hardcoded popular apps */
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_BEFORE, "hardcoded-popular");
+
 	/* set name of MetaInfo file */
 	gs_plugin_set_appstream_id (plugin, "org.gnome.Software.Plugin.Snap");
 }
