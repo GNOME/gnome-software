c477f6a889591eee45da654a95168c5f9ffe6774
Robert Ancell <robert.ancell@canonical.com>
2017-08-09 15:43:02 +1200

    snap: Only feature snaps
    Run after the other plugins that populate featured/popular apps and remove
    them when we set ours.

--- plugins/snap/gs-plugin-snap.c
+++ plugins/snap/gs-plugin-snap.c
@@ -79,11 +79,13 @@ gs_plugin_initialize (GsPlugin *plugin)
 
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "desktop-categories");
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "ubuntu-reviews");
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "appstream");
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_BETTER_THAN, "packagekit");
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_BEFORE, "icons");
 
 	/* Override hardcoded popular apps */
-	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_BEFORE, "hardcoded-popular");
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "hardcoded-popular");
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "hardcoded-featured");
 
 	/* set name of MetaInfo file */
 	gs_plugin_set_appstream_id (plugin, "org.gnome.Software.Plugin.Snap");
@@ -387,6 +389,12 @@ is_banner_icon_image (const gchar *filename)
 	return g_regex_match_simple ("^banner-icon(?:_[a-zA-Z0-9]{7})?\\.(?:png|jpg)$", filename, 0, 0);
 }
 
+static gboolean
+remove_cb (GsApp *app, gpointer user_data)
+{
+	return FALSE;
+}
+
 gboolean
 gs_plugin_add_featured (GsPlugin *plugin,
 		        GsAppList *list,
@@ -455,6 +463,8 @@ gs_plugin_add_featured (GsPlugin *plugin,
 			       background_css->str);
 	gs_app_set_metadata (app, "GnomeSoftware::FeatureTile-css", css);
 
+	/* replace any other featured apps with our one */
+	gs_app_list_filter (list, remove_cb, NULL);
 	gs_app_list_add (list, app);
 
 	return TRUE;
@@ -472,6 +482,9 @@ gs_plugin_add_popular (GsPlugin *plugin,
 	snaps = find_snaps (plugin, SNAPD_FIND_FLAGS_NONE, "featured", NULL, cancellable, error);
 	if (snaps == NULL)
 		return FALSE;
+
+	/* replace any other popular apps with our one */
+	gs_app_list_filter (list, remove_cb, NULL);
 
 	/* skip first snap - it is used as the featured app */
 	for (i = 1; i < snaps->len; i++) {
