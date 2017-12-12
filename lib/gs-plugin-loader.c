GNOME/gnome-software

commit fa7216c1284d957acbf266f879a99ffcff06abee

Cancel plugin jobs if they take too much time
Create an event and show a message in the shell to make the plugin look bad.

Fixes: https://bugzilla.gnome.org/show_bug.cgi?id=784251

lib/gs-plugin-loader.c
@@ -178,11 +178,16 @@ typedef void		 (*GsPluginAdoptAppFunc)	(GsPlugin	*plugin,
 /* async helper */
 typedef struct {
 	GsPluginLoader			*plugin_loader;
+	GCancellable			*cancellable;
+	GCancellable			*cancellable_caller;
+	gulong				 cancellable_id;
 	const gchar			*function_name;
 	const gchar			*function_name_parent;
 	GPtrArray			*catlist;
 	GsPluginJob			*plugin_job;
 	gboolean			 anything_ran;
+	guint				 timeout_id;
+	gboolean			 timeout_triggered;
 	gchar				**tokens;
 } GsPluginLoaderHelper;
 
@@ -200,9 +205,19 @@ gs_plugin_loader_helper_new (GsPluginLoader *plugin_loader, GsPluginJob *plugin_
 static void
 gs_plugin_loader_helper_free (GsPluginLoaderHelper *helper)
 {
+	if (helper->cancellable_id > 0) {
+		g_cancellable_disconnect (helper->cancellable_caller,
+					  helper->cancellable_id);
+	}
 	g_object_unref (helper->plugin_loader);
+	if (helper->timeout_id != 0)
+		g_source_remove (helper->timeout_id);
 	if (helper->plugin_job != NULL)
 		g_object_unref (helper->plugin_job);
+	if (helper->cancellable != NULL)
+		g_object_unref (helper->cancellable);
+	if (helper->cancellable_caller != NULL)
+		g_object_unref (helper->cancellable_caller);
 	if (helper->catlist != NULL)
 		g_ptr_array_unref (helper->catlist);
 	g_strfreev (helper->tokens);
@@ -359,6 +374,8 @@ gs_plugin_loader_is_error_fatal (GsPluginFailureFlags failure_flags,
 		if (g_error_matches (err, GS_PLUGIN_ERROR, GS_PLUGIN_ERROR_AUTH_INVALID))
 			return TRUE;
 	}
+	if (g_error_matches (err, GS_PLUGIN_ERROR, GS_PLUGIN_ERROR_TIMED_OUT))
+		return TRUE;
 	return FALSE;
 }
 
@@ -378,21 +395,21 @@ gs_plugin_error_handle_failure (GsPluginLoaderHelper *helper,
 		return TRUE;
 	}
 
-	/* abort early to allow main thread to process */
-	flags = gs_plugin_job_get_failure_flags (helper->plugin_job);
-	if (gs_plugin_loader_is_error_fatal (flags, error_local)) {
-		if (error != NULL)
-			*error = g_error_copy (error_local);
-		return FALSE;
-	}
-
 	/* create event which is handled by the GsShell */
+	flags = gs_plugin_job_get_failure_flags (helper->plugin_job);
 	if (flags & GS_PLUGIN_FAILURE_FLAGS_USE_EVENTS) {
 		gs_plugin_loader_create_event_from_error (helper->plugin_loader,
 							  gs_plugin_job_get_action (helper->plugin_job),
 							  plugin,
 							  gs_plugin_job_get_app (helper->plugin_job),
 							  error_local);
+	}
+
+	/* abort early to allow main thread to process */
+	if (gs_plugin_loader_is_error_fatal (flags, error_local)) {
+		if (error != NULL)
+			*error = g_error_copy (error_local);
+		return FALSE;
 	}
 
 	/* fallback to console warning */
@@ -689,7 +706,29 @@ gs_plugin_loader_call_vfunc (GsPluginLoaderHelper *helper,
 		break;
 	}
 	gs_plugin_loader_action_stop (helper->plugin_loader, plugin);
+
+	/* plugin did not return error on cancellable abort */
+	if (ret && g_cancellable_set_error_if_cancelled (cancellable, &error_local)) {
+		g_debug ("plugin did not return error with cancellable set");
+		gs_utils_error_convert_gio (&error_local);
+		ret = FALSE;
+	}
+
+	/* failed */
 	if (!ret) {
+		/* we returned cancelled, but this was because of a timeout,
+		 * so re-create error, throwing the plugin under the bus */
+		if (helper->timeout_triggered &&
+		    g_error_matches (error_local, GS_PLUGIN_ERROR, GS_PLUGIN_ERROR_CANCELLED)) {
+			g_debug ("converting cancelled to timeout");
+			g_clear_error (&error_local);
+			g_set_error (&error_local,
+				     GS_PLUGIN_ERROR,
+				     GS_PLUGIN_ERROR_TIMED_OUT,
+				     "Timeout was reached as %s took "
+				     "too long to return results",
+				     gs_plugin_get_name (plugin));
+		}
 		return gs_plugin_error_handle_failure (helper,
 							plugin,
 							error_local,
@@ -2999,6 +3038,7 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 				gs_app_set_state_recover (gs_plugin_job_get_app (helper->plugin_job));
 				gs_plugin_loader_pending_apps_remove (plugin_loader, helper);
 			}
+			gs_utils_error_convert_gio (&error);
 			g_task_return_error (task, error);
 			return;
 		}
@@ -3009,6 +3049,7 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 		helper->function_name = "gs_plugin_update_app";
 		if (!gs_plugin_loader_generic_update (plugin_loader, helper,
 						      cancellable, &error)) {
+			gs_utils_error_convert_gio (&error);
 			g_task_return_error (task, error);
 			return;
 		}
@@ -3023,6 +3064,7 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 	    !g_settings_get_boolean (priv->settings, "download-updates")) {
 		helper->function_name = "gs_plugin_add_updates_pending";
 		if (!gs_plugin_loader_run_results (helper, cancellable, &error)) {
+			gs_utils_error_convert_gio (&error);
 			g_task_return_error (task, error);
 			return;
 		}
@@ -3106,6 +3148,7 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 	/* run refine() on each one if required */
 	if (gs_plugin_job_get_refine_flags (helper->plugin_job) != 0) {
 		if (!gs_plugin_loader_run_refine (helper, list, cancellable, &error)) {
+			gs_utils_error_convert_gio (&error);
 			g_task_return_error (task, error);
 			return;
 		}
@@ -3145,6 +3188,7 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 		gs_plugin_job_set_refine_flags (helper->plugin_job,
 						GS_PLUGIN_REFINE_FLAGS_REQUIRE_ICON);
 		if (!gs_plugin_loader_run_refine (helper, list, cancellable, &error)) {
+			gs_utils_error_convert_gio (&error);
 			g_task_return_error (task, error);
 			return;
 		}
@@ -3248,6 +3292,29 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 	g_task_return_pointer (task, g_object_ref (list), (GDestroyNotify) g_object_unref);
 }
 
+static gboolean
+gs_plugin_loader_job_timeout_cb (gpointer user_data)
+{
+	GsPluginLoaderHelper *helper = (GsPluginLoaderHelper *) user_data;
+
+	/* call the cancellable */
+	g_debug ("cancelling job as it took too long");
+	if (!g_cancellable_is_cancelled (helper->cancellable))
+		g_cancellable_cancel (helper->cancellable);
+
+	/* failed */
+	helper->timeout_triggered = TRUE;
+	helper->timeout_id = 0;
+	return G_SOURCE_REMOVE;
+}
+
+static void
+gs_plugin_loader_cancelled_cb (GCancellable *cancellable, GsPluginLoaderHelper *helper)
+{
+	/* just proxy this forward */
+	g_cancellable_cancel (helper->cancellable);
+}
+
 /**
  * gs_plugin_loader_job_process_async:
  *
@@ -3264,6 +3331,7 @@ gs_plugin_loader_job_process_async (GsPluginLoader *plugin_loader,
 	GsPluginLoaderHelper *helper;
 	GsPluginLoaderPrivate *priv = gs_plugin_loader_get_instance_private (plugin_loader);
 	g_autoptr(GTask) task = NULL;
+	g_autoptr(GCancellable) cancellable_job = g_cancellable_new ();
 
 	g_return_if_fail (GS_IS_PLUGIN_LOADER (plugin_loader));
 	g_return_if_fail (GS_IS_PLUGIN_JOB (plugin_job));
@@ -3337,7 +3405,7 @@ gs_plugin_loader_job_process_async (GsPluginLoader *plugin_loader,
 	}
 
 	/* check required args */
-	task = g_task_new (plugin_loader, cancellable, callback, user_data);
+	task = g_task_new (plugin_loader, cancellable_job, callback, user_data);
 	switch (action) {
 	case GS_PLUGIN_ACTION_SEARCH:
 	case GS_PLUGIN_ACTION_SEARCH_FILES:
@@ -3398,6 +3466,10 @@ gs_plugin_loader_job_process_async (GsPluginLoader *plugin_loader,
 	g_task_set_task_data (task, helper, (GDestroyNotify) gs_plugin_loader_helper_free);
 	gs_plugin_loader_job_debug (helper);
 
+	/* let the task cancel itself */
+	g_task_set_check_cancellable (task, FALSE);
+	g_task_set_return_on_cancel (task, FALSE);
+
 	/* pre-tokenize search */
 	if (action == GS_PLUGIN_ACTION_SEARCH) {
 		const gchar *search = gs_plugin_job_get_search (plugin_job);
@@ -3409,6 +3481,36 @@ gs_plugin_loader_job_process_async (GsPluginLoader *plugin_loader,
 						 "failed to tokenize %s", search);
 			return;
 		}
+	}
+
+	/* jobs always have a valid cancellable, so proxy the caller */
+	helper->cancellable = g_object_ref (cancellable_job);
+	if (cancellable != NULL) {
+		helper->cancellable_caller = g_object_ref (cancellable);
+		helper->cancellable_id =
+			g_cancellable_connect (helper->cancellable_caller,
+					       G_CALLBACK (gs_plugin_loader_cancelled_cb),
+					       helper, NULL);
+	}
+
+	/* set up a hang handler */
+	switch (action) {
+	case GS_PLUGIN_ACTION_GET_CATEGORY_APPS:
+	case GS_PLUGIN_ACTION_GET_FEATURED:
+	case GS_PLUGIN_ACTION_GET_INSTALLED:
+	case GS_PLUGIN_ACTION_GET_POPULAR:
+	case GS_PLUGIN_ACTION_GET_RECENT:
+	case GS_PLUGIN_ACTION_GET_UPDATES:
+	case GS_PLUGIN_ACTION_SEARCH:
+	case GS_PLUGIN_ACTION_SEARCH_FILES:
+	case GS_PLUGIN_ACTION_SEARCH_PROVIDES:
+		helper->timeout_id =
+			g_timeout_add_seconds (gs_plugin_job_get_timeout (plugin_job),
+					       gs_plugin_loader_job_timeout_cb,
+					       helper);
+		break;
+	default:
+		break;
 	}
 
 	/* run in a thread */

lib/gs-plugin-job-private.h
@@ -39,6 +39,7 @@ void			 gs_plugin_job_remove_refine_flags	(GsPluginJob	*self,
 								 GsPluginRefineFlags refine_flags);
 GsPluginFailureFlags	 gs_plugin_job_get_failure_flags	(GsPluginJob	*self);
 guint			 gs_plugin_job_get_max_results		(GsPluginJob	*self);
+guint			 gs_plugin_job_get_timeout		(GsPluginJob	*self);
 guint64			 gs_plugin_job_get_age			(GsPluginJob	*self);
 GsAppListSortFunc	 gs_plugin_job_get_sort_func		(GsPluginJob	*self);
 gpointer		 gs_plugin_job_get_sort_func_data	(GsPluginJob	*self);

lib/gs-plugin-job.c
@@ -33,6 +33,7 @@ struct _GsPluginJob
 	GsPluginRefreshFlags	 refresh_flags;
 	GsPluginFailureFlags	 failure_flags;
 	guint			 max_results;
+	guint			 timeout;
 	guint64			 age;
 	GsPlugin		*plugin;
 	GsPluginAction		 action;
@@ -64,6 +65,7 @@ enum {
 	PROP_REVIEW,
 	PROP_MAX_RESULTS,
 	PROP_PRICE,
+	PROP_TIMEOUT,
 	PROP_LAST
 };
 
@@ -83,6 +85,8 @@ gs_plugin_job_to_string (GsPluginJob *self)
 		g_autofree gchar *tmp = gs_plugin_failure_flags_to_string (self->failure_flags);
 		g_string_append_printf (str, " with failure-flags=%s", tmp);
 	}
+	if (self->timeout > 0)
+		g_string_append_printf (str, " with timeout=%u", self->timeout);
 	if (self->age != 0) {
 		if (self->age == G_MAXUINT) {
 			g_string_append (str, " with cache age=any");
@@ -215,6 +219,20 @@ gs_plugin_job_get_max_results (GsPluginJob *self)
 {
 	g_return_val_if_fail (GS_IS_PLUGIN_JOB (self), 0);
 	return self->max_results;
+}
+
+void
+gs_plugin_job_set_timeout (GsPluginJob *self, guint timeout)
+{
+	g_return_if_fail (GS_IS_PLUGIN_JOB (self));
+	self->timeout = timeout;
+}
+
+guint
+gs_plugin_job_get_timeout (GsPluginJob *self)
+{
+	g_return_val_if_fail (GS_IS_PLUGIN_JOB (self), 0);
+	return self->timeout;
 }
 
 void
@@ -454,6 +472,9 @@ gs_plugin_job_get_property (GObject *obj, guint prop_id, GValue *value, GParamSp
 	case PROP_MAX_RESULTS:
 		g_value_set_uint (value, self->max_results);
 		break;
+	case PROP_TIMEOUT:
+		g_value_set_uint (value, self->timeout);
+		break;
 	default:
 		G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
 		break;
@@ -504,6 +525,9 @@ gs_plugin_job_set_property (GObject *obj, guint prop_id, const GValue *value, GP
 		break;
 	case PROP_MAX_RESULTS:
 		gs_plugin_job_set_max_results (self, g_value_get_uint (value));
+		break;
+	case PROP_TIMEOUT:
+		gs_plugin_job_set_timeout (self, g_value_get_uint (value));
 		break;
 	case PROP_PRICE:
 		gs_plugin_job_set_price (self, g_value_get_object (value));
@@ -605,6 +629,11 @@ gs_plugin_job_class_init (GsPluginJobClass *klass)
 				   0, G_MAXUINT, 0,
 				   G_PARAM_READWRITE);
 	g_object_class_install_property (object_class, PROP_MAX_RESULTS, pspec);
+
+	pspec = g_param_spec_uint ("timeout", NULL, NULL,
+				   0, G_MAXUINT, 60,
+				   G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+	g_object_class_install_property (object_class, PROP_TIMEOUT, pspec);
 
 	pspec = g_param_spec_object ("price", NULL, NULL,
 				     GS_TYPE_PRICE,

lib/gs-plugin-job.h
@@ -44,6 +44,8 @@ void		 gs_plugin_job_set_failure_flags	(GsPluginJob	*self,
 							 GsPluginFailureFlags failure_flags);
 void		 gs_plugin_job_set_max_results		(GsPluginJob	*self,
 							 guint		 max_results);
+void		 gs_plugin_job_set_timeout		(GsPluginJob	*self,
+							 guint		 timeout);
 void		 gs_plugin_job_set_age			(GsPluginJob	*self,
 							 guint64	 age);
 void		 gs_plugin_job_set_sort_func		(GsPluginJob	*self,

lib/gs-plugin-types.h
@@ -91,6 +91,7 @@ typedef guint64 GsPluginFlags;
  * @GS_PLUGIN_ERROR_DELETE_FAILED:		The delete action failed
  * @GS_PLUGIN_ERROR_RESTART_REQUIRED:		A restart is required
  * @GS_PLUGIN_ERROR_AC_POWER_REQUIRED:		AC power is required
+ * @GS_PLUGIN_ERROR_TIMED_OUT:			The job timed out
  *
  * The failure error types.
  **/
@@ -113,6 +114,7 @@ typedef enum {
 	GS_PLUGIN_ERROR_DELETE_FAILED,
 	GS_PLUGIN_ERROR_RESTART_REQUIRED,
 	GS_PLUGIN_ERROR_AC_POWER_REQUIRED,
+	GS_PLUGIN_ERROR_TIMED_OUT,
 	/*< private >*/
 	GS_PLUGIN_ERROR_LAST
 } GsPluginError;

lib/gs-plugin.c
@@ -1664,6 +1664,8 @@ gs_plugin_error_to_string (GsPluginError error)
 		return "restart-required";
 	if (error == GS_PLUGIN_ERROR_AC_POWER_REQUIRED)
 		return "ac-power-required";
+	if (error == GS_PLUGIN_ERROR_TIMED_OUT)
+		return "timed-out";
 	return NULL;
 }
 

lib/gs-utils.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2013-2016 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2013-2017 Richard Hughes <richard@hughsie.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -678,10 +678,12 @@ gs_utils_error_convert_gio (GError **perror)
 		return FALSE;
 	switch (error->code) {
 	case G_IO_ERROR_FAILED:
-	case G_IO_ERROR_TIMED_OUT:
 	case G_IO_ERROR_NOT_FOUND:
 	case G_IO_ERROR_EXISTS:
 		error->code = GS_PLUGIN_ERROR_FAILED;
+		break;
+	case G_IO_ERROR_TIMED_OUT:
+		error->code = GS_PLUGIN_ERROR_TIMED_OUT;
 		break;
 	case G_IO_ERROR_NOT_SUPPORTED:
 		error->code = GS_PLUGIN_ERROR_NOT_SUPPORTED;

plugins/dummy/gs-plugin-dummy.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2011-2013 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2011-2017 Richard Hughes <richard@hughsie.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -224,6 +224,64 @@ gs_plugin_url_to_app (GsPlugin *plugin,
 	return TRUE;
 }
 
+typedef struct {
+	GMainLoop	*loop;
+	GCancellable	*cancellable;
+	guint		 timer_id;
+	gulong		 cancellable_id;
+} GsPluginDummyTimeoutHelper;
+
+static gboolean
+gs_plugin_dummy_timeout_hang_cb (gpointer user_data)
+{
+	GsPluginDummyTimeoutHelper *helper = (GsPluginDummyTimeoutHelper *) user_data;
+	helper->timer_id = 0;
+	g_debug ("timeout hang");
+	g_main_loop_quit (helper->loop);
+	return FALSE;
+}
+
+static void
+gs_plugin_dummy_timeout_cancelled_cb (GCancellable *cancellable, gpointer user_data)
+{
+	GsPluginDummyTimeoutHelper *helper = (GsPluginDummyTimeoutHelper *) user_data;
+	g_debug ("calling cancel");
+	g_main_loop_quit (helper->loop);
+}
+
+static void
+gs_plugin_dummy_timeout_helper_free (GsPluginDummyTimeoutHelper *helper)
+{
+	if (helper->cancellable_id != 0)
+		g_signal_handler_disconnect (helper->cancellable, helper->cancellable_id);
+	if (helper->timer_id != 0)
+		g_source_remove (helper->timer_id);
+	if (helper->cancellable != NULL)
+		g_object_unref (helper->cancellable);
+	g_main_loop_unref (helper->loop);
+	g_free (helper);
+}
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GsPluginDummyTimeoutHelper, gs_plugin_dummy_timeout_helper_free)
+
+static void
+gs_plugin_dummy_timeout_add (guint timeout_ms, GCancellable *cancellable)
+{
+	g_autoptr(GsPluginDummyTimeoutHelper) helper = g_new0 (GsPluginDummyTimeoutHelper, 1);
+	helper->loop = g_main_loop_new (NULL, TRUE);
+	if (cancellable != NULL) {
+		helper->cancellable = g_object_ref (cancellable);
+		helper->cancellable_id =
+			g_signal_connect (cancellable, "cancelled",
+					  G_CALLBACK (gs_plugin_dummy_timeout_cancelled_cb),
+					  helper);
+	}
+	helper->timer_id = g_timeout_add (timeout_ms,
+					  gs_plugin_dummy_timeout_hang_cb,
+					  helper);
+	g_main_loop_run (helper->loop);
+}
+
 gboolean
 gs_plugin_add_search (GsPlugin *plugin,
 		      gchar **values,
@@ -234,6 +292,16 @@ gs_plugin_add_search (GsPlugin *plugin,
 	GsPluginData *priv = gs_plugin_get_data (plugin);
 	g_autoptr(GsApp) app = NULL;
 	g_autoptr(AsIcon) ic = NULL;
+
+	/* hang the plugin for 5 seconds */
+	if (g_strcmp0 (values[0], "hang") == 0) {
+		gs_plugin_dummy_timeout_add (5000, cancellable);
+		if (g_cancellable_set_error_if_cancelled (cancellable, error)) {
+			gs_utils_error_convert_gio (error);
+			return FALSE;
+		}
+		return TRUE;
+	}
 
 	/* we're very specific */
 	if (g_strcmp0 (values[0], "chiron") != 0)

commit ca1fc8a617ffa34b5dea2f9b5fc8b15aec76b776

plugin loader: Fix a race with thread cancelling
In our UI code, a common pattern is to cancel async operation
cancellables in dispose, and then rely on the callbacks to return
immediately with CANCELLED error code. This pattern makes it easy to
short circuit when CANCELLED is set and avoid accessing private data
that has been released in dispose.

Commit fa7216c1284d957acbf266f879a99ffcff06abee regressed this by
changing the tasks created in the plugin loader to not have
return-on-cancel set, which causes the async operations to not return
synchronously, breaking this UI pattern.

This commit changes it back so that plugin loader async operations
return synchronously when the caller cancels the cancellable.

https://bugzilla.redhat.com/show_bug.cgi?id=1467499

lib/gs-plugin-loader.c
@@ -1626,6 +1626,7 @@ gs_plugin_loader_job_get_categories_async (GsPluginLoader *plugin_loader,
 	/* run in a thread */
 	task = g_task_new (plugin_loader, cancellable, callback, user_data);
 	g_task_set_task_data (task, helper, (GDestroyNotify) gs_plugin_loader_helper_free);
+	g_task_set_return_on_cancel (task, TRUE);
 	g_task_run_in_thread (task, gs_plugin_loader_job_get_categories_thread_cb);
 }
 
@@ -3565,10 +3566,6 @@ gs_plugin_loader_job_process_async (GsPluginLoader *plugin_loader,
 	helper = gs_plugin_loader_helper_new (plugin_loader, plugin_job);
 	g_task_set_task_data (task, helper, (GDestroyNotify) gs_plugin_loader_helper_free);
 
-	/* let the task cancel itself */
-	g_task_set_check_cancellable (task, FALSE);
-	g_task_set_return_on_cancel (task, FALSE);
-
 	/* pre-tokenize search */
 	if (action == GS_PLUGIN_ACTION_SEARCH) {
 		const gchar *search = gs_plugin_job_get_search (plugin_job);
@@ -3612,6 +3609,7 @@ gs_plugin_loader_job_process_async (GsPluginLoader *plugin_loader,
 	}
 
 	/* run in a thread */
+	g_task_set_return_on_cancel (task, TRUE);
 	g_task_run_in_thread (task, gs_plugin_loader_process_thread_cb);
 }
 