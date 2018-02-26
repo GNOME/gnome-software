b80775cf5820869929fd99dd10c5c8cf3bede864
Kalev Lember <klember@redhat.com>
2018-02-26 16:13:15 +0100

    app row: Fix reversed logic for making buttons insensitive
    It only makes sense to make the Install/Remove buttons insensitive while
    we're in INSTALLING or REMOVING state.

--- src/gs-app-row.c
+++ src/gs-app-row.c
@@ -274,11 +274,11 @@ gs_app_row_refresh_button (GsAppRow *app_row, gboolean missing_search_result)
 		switch (gs_app_get_state (priv->app)) {
 		case AS_APP_STATE_INSTALLING:
 		case AS_APP_STATE_REMOVING:
-			gtk_widget_set_sensitive (priv->button, FALSE);
-			break;
-		default:
 			gtk_widget_set_sensitive (priv->button,
 						  gs_app_get_allow_cancel (priv->app));
+			break;
+		default:
+			gtk_widget_set_sensitive (priv->button, TRUE);
 			break;
 		}
 	}
