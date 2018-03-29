68f4c49157188e499bd36ef6e56239c78be5cc09
Kalev Lember <klember@redhat.com>
2018-03-29 21:41:08 +0200

    rpm-ostree: Ensure D-Bus properties are updated before reading them
    Use new Reload() API in rpm-ostree 2018.4 to make sure the daemon emits
    all D-Bus properties before reading them.

--- plugins/rpm-ostree/gs-plugin-rpm-ostree.c
+++ plugins/rpm-ostree/gs-plugin-rpm-ostree.c
@@ -452,6 +452,9 @@ gs_plugin_add_updates (GsPlugin *plugin,
 	const gchar *version = NULL;
 	g_auto(GVariantDict) cached_update_dict;
 
+	/* ensure D-Bus properties are updated before reading them */
+	gs_rpmostree_sysroot_call_reload_sync (priv->sysroot_proxy, cancellable, error);
+
 	cached_update = gs_rpmostree_os_dup_cached_update (priv->os_proxy);
 	g_variant_dict_init (&cached_update_dict, cached_update);
 
@@ -657,6 +660,9 @@ gs_plugin_refine (GsPlugin *plugin,
 	g_autoptr(GVariant) booted_deployment = NULL;
 	g_auto(GStrv) layered_packages = NULL;
 	g_autofree gchar *checksum = NULL;
+
+	/* ensure D-Bus properties are updated before reading them */
+	gs_rpmostree_sysroot_call_reload_sync (priv->sysroot_proxy, cancellable, error);
 
 	booted_deployment = gs_rpmostree_os_dup_booted_deployment (priv->os_proxy);
 	g_assert (g_variant_lookup (booted_deployment,
