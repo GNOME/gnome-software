8ae4933c931ced71aca4fb869c1694e232ba0582
Richard Hughes <richard@hughsie.com>
2018-07-05 12:14:32 +0100

    Add gs_app_remove_kudo()

--- lib/gs-app.h
+++ lib/gs-app.h
@@ -310,6 +310,8 @@ void		 gs_app_set_keywords		(GsApp		*app,
 						 GPtrArray	*keywords);
 void		 gs_app_add_kudo		(GsApp		*app,
 						 GsAppKudo	 kudo);
+void		 gs_app_remove_kudo		(GsApp		*app,
+						 GsAppKudo	 kudo);
 gboolean	 gs_app_has_kudo		(GsApp		*app,
 						 GsAppKudo	 kudo);
 guint64		 gs_app_get_kudos		(GsApp		*app);
