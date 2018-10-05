GNOME/gnome-software

commit 9df8f0a60fb9766b56f8029b86d98db47b33a599

Download applications explicitly when required
This adds gs_plugin_update(), but removes gs_plugin_add_updates_pending() and
GsPluginRefreshFlags. The idea is that the frontend can ask the plugin loader
to download specific GsApp objects, rather than trying to wedge this into the
refresh action as some kind of payload.

Breaks internal plugin API, but makes everything simpler, which is fine with me.

plugins/rpm-ostree/gs-plugin-rpm-ostree.c
@@ -408,73 +408,89 @@ make_rpmostree_options_variant (gboolean reboot,
 	return g_variant_ref_sink (g_variant_dict_end (&dict));
 }
 
+static gboolean
+_download_only (GsPlugin *plugin, GCancellable *cancellable, GError **error)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+	g_autofree gchar *transaction_address = NULL;
+	g_autoptr(GVariant) options = NULL;
+
+	options = make_rpmostree_options_variant (FALSE,  /* reboot */
+						  FALSE,  /* allow-downgrade */
+						  FALSE,  /* cache-only */
+						  TRUE,   /* download-only */
+						  FALSE,  /* skip-purge */
+						  FALSE,  /* no-pull-base */
+						  FALSE,  /* dry-run */
+						  FALSE); /* no-overrides */
+	if (!gs_rpmostree_os_call_upgrade_sync (priv->os_proxy,
+						options,
+						NULL /* fd list */,
+						&transaction_address,
+						NULL /* fd list out */,
+						cancellable,
+						error)) {
+		gs_utils_error_convert_gio (error);
+		return FALSE;
+	}
+
+	if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
+							 transaction_address,
+							 cancellable,
+							 error)) {
+		gs_utils_error_convert_gio (error);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_download (GsPlugin *plugin,
+		    GsAppList *list,
+		    GCancellable *cancellable,
+		    GError **error)
+{
+	/* any are us? */
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (g_strcmp0 (gs_app_get_management_plugin (app), "rpm-ostree") == 0)
+			return _download_only (plugin, cancellable, error);
+	}
+	return TRUE;
+}
+
 gboolean
 gs_plugin_refresh (GsPlugin *plugin,
                    guint cache_age,
-                   GsPluginRefreshFlags flags,
                    GCancellable *cancellable,
                    GError **error)
 {
 	GsPluginData *priv = gs_plugin_get_data (plugin);
+	GVariantDict dict;
+	g_autofree gchar *transaction_address = NULL;
+	g_autoptr(GVariant) options = NULL;
 
-	if (flags & GS_PLUGIN_REFRESH_FLAGS_PAYLOAD) {
-		g_autofree gchar *transaction_address = NULL;
-		g_autoptr(GVariant) options = NULL;
+	g_variant_dict_init (&dict, NULL);
+	g_variant_dict_insert (&dict, "mode", "s", "check");
+	options = g_variant_ref_sink (g_variant_dict_end (&dict));
 
-		options = make_rpmostree_options_variant (FALSE,  /* reboot */
-		                                          FALSE,  /* allow-downgrade */
-		                                          FALSE,  /* cache-only */
-		                                          TRUE,   /* download-only */
-		                                          FALSE,  /* skip-purge */
-		                                          FALSE,  /* no-pull-base */
-		                                          FALSE,  /* dry-run */
-		                                          FALSE); /* no-overrides */
-		if (!gs_rpmostree_os_call_upgrade_sync (priv->os_proxy,
-		                                        options,
-		                                        NULL /* fd list */,
-		                                        &transaction_address,
-		                                        NULL /* fd list out */,
-		                                        cancellable,
-		                                        error)) {
-			gs_utils_error_convert_gio (error);
-			return FALSE;
-		}
-
-		if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
-		                                                 transaction_address,
-		                                                 cancellable,
-		                                                 error)) {
-			gs_utils_error_convert_gio (error);
-			return FALSE;
-		}
+	if (!gs_rpmostree_os_call_automatic_update_trigger_sync (priv->os_proxy,
+								 options,
+								 NULL,
+								 &transaction_address,
+								 cancellable,
+								 error)) {
+		gs_utils_error_convert_gio (error);
+		return FALSE;
 	}
 
-	if (flags & GS_PLUGIN_REFRESH_FLAGS_METADATA) {
-		g_autofree gchar *transaction_address = NULL;
-		g_autoptr(GVariant) options = NULL;
-		GVariantDict dict;
-
-		g_variant_dict_init (&dict, NULL);
-		g_variant_dict_insert (&dict, "mode", "s", "check");
-		options = g_variant_ref_sink (g_variant_dict_end (&dict));
-
-		if (!gs_rpmostree_os_call_automatic_update_trigger_sync (priv->os_proxy,
-		                                                         options,
-		                                                         NULL,
-		                                                         &transaction_address,
-		                                                         cancellable,
-		                                                         error)) {
-			gs_utils_error_convert_gio (error);
-			return FALSE;
-		}
-
-		if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
-		                                                 transaction_address,
-		                                                 cancellable,
-		                                                 error)) {
-			gs_utils_error_convert_gio (error);
-			return FALSE;
-		}
+	if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
+							 transaction_address,
+							 cancellable,
+							 error)) {
+		gs_utils_error_convert_gio (error);
+		return FALSE;
 	}
 
 	/* update UI */

data/org.gnome.software.gschema.xml
@@ -12,8 +12,13 @@
     </key>
     <key name="download-updates" type="b">
       <default>true</default>
-      <summary>Whether to automatically download updates</summary>
-      <description>If enabled, GNOME Software automatically downloads updates in the background and prompts the user to install them when ready.</description>
+      <summary>Automatically download and install updates</summary>
+      <description>If enabled, GNOME Software automatically downloads software updates in the background, also installing ones that do not require a reboot.</description>
+    </key>
+    <key name="download-updates-notify" type="b">
+      <default>true</default>
+      <summary>Notify the user about software updated in the background</summary>
+      <description>If enabled, GNOME Software notifies the user about updates that happened whilst the user was idle.</description>
     </key>
     <key name="refresh-when-metered" type="b">
       <default>false</default>

doc/api/gnome-software-docs.xml
@@ -335,15 +335,16 @@ gs_plugin_add_installed (GsPlugin *plugin,
           On fast connections we should only show that for a couple of seconds,
           but it's a good idea to try any avoid that if at all possible in the
           plugin.
-          Once per day the <code>gs_plugin_refresh()</code> method is called again,
-          but this time with <code>GS_PLUGIN_REFRESH_FLAGS_PAYLOAD</code> set.
+          Once per day the <code>gs_plugin_get_updates()</code> method is called,
+          and then <code>gs_plugin_download_app()</code> may be called if the
+          user has configured automatic updates.
           This is where the Flatpak plugin would download any ostree trees (but
           not doing the deploy step) so that the applications can be updated live
           in the details panel without having to wait for the download to complete.
           In a similar way, the fwupd plugin downloads the tiny LVFS metadata with
-          <code>GS_PLUGIN_REFRESH_FLAGS_METADATA</code> and then downloads the
-          large firmware files themselves only when the
-          <code>GS_PLUGIN_REFRESH_FLAGS_PAYLOAD</code> flag is set.
+          <code>gs_plugin_refresh()</code> and then downloads the large firmware
+          files themselves when <code>gs_plugin_download_app()</code> or
+          <code>gs_plugin_download_app()</code> is called.
         </para>
         <para>
           If the <code>@app</code> parameter is set for
@@ -371,35 +372,25 @@ gs_plugin_add_installed (GsPlugin *plugin,
 gboolean
 gs_plugin_refresh (GsPlugin *plugin,
                    guint cache_age,
-                   GsPluginRefreshFlags flags,
                    GCancellable *cancellable,
                    GError **error)
 {
   const gchar *metadata_fn = "/var/cache/example/metadata.xml";
   const gchar *metadata_url = "http://www.example.com/new.xml";
+  g_autoptr(GFile) file = g_file_new_for_path (metadata_fn);
 
-  /* this is called at startup and once per day */
-  if (flags &amp; GS_PLUGIN_REFRESH_FLAGS_METADATA) {
-    g_autoptr(GFile) file = g_file_new_for_path (metadata_fn);
-
-    /* is the metadata missing or too old */
-    if (gs_utils_get_file_age (file) &gt; cache_age) {
-      if (!gs_plugin_download_file (plugin,
-                                    NULL,
-                                    metadata_url,
-                                    metadata_fn,
-                                    cancellable,
-                                    error)) {
-        /* it's okay to fail here */
-        return FALSE;
-      }
-      g_debug ("successfully downloaded new metadata");
+  /* is the metadata missing or too old */
+  if (gs_utils_get_file_age (file) &gt; cache_age) {
+    if (!gs_plugin_download_file (plugin,
+                                  NULL,
+                                  metadata_url,
+                                  metadata_fn,
+                                  cancellable,
+                                  error)) {
+      /* it's okay to fail here */
+      return FALSE;
     }
-  }
-
-  /* this is called when the session is idle */
-  if ((flags &amp; GS_PLUGIN_REFRESH_FLAGS_PAYLOAD) == 0) {
-    // FIXME: download any required updates now
+    g_debug ("successfully downloaded new metadata");
   }
   return TRUE;
 }

lib/gs-cmd.c
@@ -271,18 +271,6 @@ gs_cmd_action_exec (GsCmdSelf *self, GsPluginAction action, const gchar *name, G
 					    NULL, error);
 }
 
-static GsPluginRefreshFlags
-gs_cmd_refresh_flag_from_string (const gchar *flag)
-{
-	if (flag == NULL || g_strcmp0 (flag, "all") == 0)
-		return G_MAXINT32;
-	if (g_strcmp0 (flag, "metadata") == 0)
-		return GS_PLUGIN_REFRESH_FLAGS_METADATA;
-	if (g_strcmp0 (flag, "payload") == 0)
-		return GS_PLUGIN_REFRESH_FLAGS_PAYLOAD;
-	return GS_PLUGIN_REFRESH_FLAGS_NONE;
-}
-
 static void
 gs_cmd_self_free (GsCmdSelf *self)
 {
@@ -404,7 +392,6 @@ main (int argc, char **argv)
 		g_autoptr(GsPluginJob) plugin_job = NULL;
 		plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_REFRESH,
 						 "age", (guint64) G_MAXUINT,
-						 "refresh-flags", GS_PLUGIN_REFRESH_FLAGS_METADATA,
 						 NULL);
 		ret = gs_plugin_loader_job_action (self->plugin_loader, plugin_job,
 						    NULL, &error);
@@ -666,12 +653,9 @@ main (int argc, char **argv)
 			}
 		}
 	} else if (argc >= 2 && g_strcmp0 (argv[1], "refresh") == 0) {
-		GsPluginRefreshFlags refresh_flags;
 		g_autoptr(GsPluginJob) plugin_job = NULL;
-		refresh_flags = gs_cmd_refresh_flag_from_string (argv[2]);
 		plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_REFRESH,
 						 "age", (guint64) cache_age,
-						 "refresh-flags", refresh_flags,
 						 NULL);
 		ret = gs_plugin_loader_job_action (self->plugin_loader, plugin_job,
 						    NULL, &error);

lib/gs-plugin-job-private.h
@@ -29,7 +29,6 @@
 G_BEGIN_DECLS
 
 GsPluginAction		 gs_plugin_job_get_action		(GsPluginJob	*self);
-GsPluginRefreshFlags	 gs_plugin_job_get_refresh_flags	(GsPluginJob	*self);
 GsPluginRefineFlags	 gs_plugin_job_get_filter_flags		(GsPluginJob	*self);
 GsPluginRefineFlags	 gs_plugin_job_get_refine_flags		(GsPluginJob	*self);
 gboolean		 gs_plugin_job_has_refine_flags		(GsPluginJob	*self,

lib/gs-plugin-job.c
@@ -32,7 +32,6 @@ struct _GsPluginJob
 	GObject			 parent_instance;
 	GsPluginRefineFlags	 refine_flags;
 	GsPluginRefineFlags	 filter_flags;
-	GsPluginRefreshFlags	 refresh_flags;
 	gboolean		 interactive;
 	guint			 max_results;
 	guint			 timeout;
@@ -59,7 +58,6 @@ enum {
 	PROP_SEARCH,
 	PROP_REFINE_FLAGS,
 	PROP_FILTER_FLAGS,
-	PROP_REFRESH_FLAGS,
 	PROP_INTERACTIVE,
 	PROP_AUTH,
 	PROP_APP,
@@ -183,20 +181,6 @@ gs_plugin_job_get_filter_flags (GsPluginJob *self)
 {
 	g_return_val_if_fail (GS_IS_PLUGIN_JOB (self), 0);
 	return self->filter_flags;
-}
-
-void
-gs_plugin_job_set_refresh_flags (GsPluginJob *self, GsPluginRefreshFlags refresh_flags)
-{
-	g_return_if_fail (GS_IS_PLUGIN_JOB (self));
-	self->refresh_flags = refresh_flags;
-}
-
-GsPluginRefreshFlags
-gs_plugin_job_get_refresh_flags (GsPluginJob *self)
-{
-	g_return_val_if_fail (GS_IS_PLUGIN_JOB (self), 0);
-	return self->refresh_flags;
 }
 
 gboolean
@@ -469,9 +453,6 @@ gs_plugin_job_get_property (GObject *obj, guint prop_id, GValue *value, GParamSp
 	case PROP_FILTER_FLAGS:
 		g_value_set_uint64 (value, self->filter_flags);
 		break;
-	case PROP_REFRESH_FLAGS:
-		g_value_set_uint64 (value, self->refresh_flags);
-		break;
 	case PROP_INTERACTIVE:
 		g_value_set_uint64 (value, self->interactive);
 		break;
@@ -528,9 +509,6 @@ gs_plugin_job_set_property (GObject *obj, guint prop_id, const GValue *value, GP
 		break;
 	case PROP_FILTER_FLAGS:
 		gs_plugin_job_set_filter_flags (self, g_value_get_uint64 (value));
-		break;
-	case PROP_REFRESH_FLAGS:
-		gs_plugin_job_set_refresh_flags (self, g_value_get_uint64 (value));
 		break;
 	case PROP_INTERACTIVE:
 		gs_plugin_job_set_interactive (self, g_value_get_uint64 (value));
@@ -618,11 +596,6 @@ gs_plugin_job_class_init (GsPluginJobClass *klass)
 				     G_PARAM_READWRITE);
 	g_object_class_install_property (object_class, PROP_FILTER_FLAGS, pspec);
 
-	pspec = g_param_spec_uint64 ("refresh-flags", NULL, NULL,
-				     0, G_MAXUINT64, 0,
-				     G_PARAM_READWRITE);
-	g_object_class_install_property (object_class, PROP_REFRESH_FLAGS, pspec);
-
 	pspec = g_param_spec_uint64 ("interactive", NULL, NULL,
 				     0, G_MAXUINT64, 0,
 				     G_PARAM_READWRITE);
@@ -684,7 +657,6 @@ gs_plugin_job_init (GsPluginJob *self)
 {
 	self->refine_flags = GS_PLUGIN_REFINE_FLAGS_DEFAULT;
 	self->filter_flags = GS_PLUGIN_REFINE_FLAGS_DEFAULT;
-	self->refresh_flags = GS_PLUGIN_REFRESH_FLAGS_NONE;
 	self->list = gs_app_list_new ();
 	self->time_created = g_get_monotonic_time ();
 }

lib/gs-plugin-job.h
@@ -40,8 +40,6 @@ void		 gs_plugin_job_set_refine_flags		(GsPluginJob	*self,
 							 GsPluginRefineFlags refine_flags);
 void		 gs_plugin_job_set_filter_flags		(GsPluginJob	*self,
 							 GsPluginRefineFlags filter_flags);
-void		 gs_plugin_job_set_refresh_flags	(GsPluginJob	*self,
-							 GsPluginRefreshFlags refresh_flags);
 void		 gs_plugin_job_set_interactive		(GsPluginJob	*self,
 							 gboolean	 interactive);
 void		 gs_plugin_job_set_max_results		(GsPluginJob	*self,

lib/gs-plugin-loader.c
@@ -161,7 +161,6 @@ typedef gboolean	 (*GsPluginRefineWildcardFunc)	(GsPlugin	*plugin,
 							 GError		**error);
 typedef gboolean	 (*GsPluginRefreshFunc)		(GsPlugin	*plugin,
 							 guint		 cache_age,
-							 GsPluginRefreshFlags refresh_flags,
 							 GCancellable	*cancellable,
 							 GError		**error);
 typedef gboolean	 (*GsPluginFileToAppFunc)	(GsPlugin	*plugin,
@@ -216,6 +215,7 @@ gs_plugin_loader_helper_free (GsPluginLoaderHelper *helper)
 	case GS_PLUGIN_ACTION_INSTALL:
 	case GS_PLUGIN_ACTION_REMOVE:
 	case GS_PLUGIN_ACTION_UPDATE:
+	case GS_PLUGIN_ACTION_DOWNLOAD:
 		{
 			GsApp *app = gs_plugin_job_get_app (helper->plugin_job);
 			if (app != NULL)
@@ -597,6 +597,19 @@ gs_plugin_loader_call_vfunc (GsPluginLoaderHelper *helper,
 				    gs_plugin_action_to_string (action));
 		}
 		break;
+	case GS_PLUGIN_ACTION_DOWNLOAD:
+		if (g_strcmp0 (helper->function_name, "gs_plugin_download_app") == 0) {
+			GsPluginActionFunc plugin_func = func;
+			ret = plugin_func (plugin, app, cancellable, &error_local);
+		} else if (g_strcmp0 (helper->function_name, "gs_plugin_download") == 0) {
+			GsPluginUpdateFunc plugin_func = func;
+			ret = plugin_func (plugin, list, cancellable, &error_local);
+		} else {
+			g_critical ("function_name %s invalid for %s",
+				    helper->function_name,
+				    gs_plugin_action_to_string (action));
+		}
+		break;
 	case GS_PLUGIN_ACTION_INSTALL:
 	case GS_PLUGIN_ACTION_REMOVE:
 	case GS_PLUGIN_ACTION_SET_RATING:
@@ -690,7 +703,6 @@ gs_plugin_loader_call_vfunc (GsPluginLoaderHelper *helper,
 			GsPluginRefreshFunc plugin_func = func;
 			ret = plugin_func (plugin,
 					   gs_plugin_job_get_age (helper->plugin_job),
-					   gs_plugin_job_get_refresh_flags (helper->plugin_job),
 					   cancellable, &error_local);
 		}
 		break;
@@ -3089,7 +3101,6 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 	GsAppList *list = gs_plugin_job_get_list (helper->plugin_job);
 	GsPluginAction action = gs_plugin_job_get_action (helper->plugin_job);
 	GsPluginLoader *plugin_loader = GS_PLUGIN_LOADER (object);
-	GsPluginLoaderPrivate *priv = gs_plugin_loader_get_instance_private (plugin_loader);
 	GsPluginRefineFlags filter_flags;
 	GsPluginRefineFlags refine_flags;
 	gboolean add_to_pending_array = FALSE;
@@ -3130,22 +3141,19 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 			g_task_return_error (task, error);
 			return;
 		}
-	}
-
-	/* remove from pending list */
-	if (add_to_pending_array)
-		gs_plugin_loader_pending_apps_remove (plugin_loader, helper);
-
-	/* append extra things when we want the list of pending updates */
-	if (action == GS_PLUGIN_ACTION_GET_UPDATES &&
-	    !g_settings_get_boolean (priv->settings, "download-updates")) {
-		helper->function_name = "gs_plugin_add_updates_pending";
-		if (!gs_plugin_loader_run_results (helper, cancellable, &error)) {
+	} else if (action == GS_PLUGIN_ACTION_DOWNLOAD) {
+		helper->function_name = "gs_plugin_download_app";
+		if (!gs_plugin_loader_generic_update (plugin_loader, helper,
+						      cancellable, &error)) {
 			gs_utils_error_convert_gio (&error);
 			g_task_return_error (task, error);
 			return;
 		}
 	}
+
+	/* remove from pending list */
+	if (add_to_pending_array)
+		gs_plugin_loader_pending_apps_remove (plugin_loader, helper);
 
 	/* some functions are really required for proper operation */
 	switch (action) {
@@ -3154,6 +3162,7 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 	case GS_PLUGIN_ACTION_GET_UPDATES:
 	case GS_PLUGIN_ACTION_INITIALIZE:
 	case GS_PLUGIN_ACTION_INSTALL:
+	case GS_PLUGIN_ACTION_DOWNLOAD:
 	case GS_PLUGIN_ACTION_LAUNCH:
 	case GS_PLUGIN_ACTION_REFRESH:
 	case GS_PLUGIN_ACTION_REMOVE:

commit 5cdee7f547199c6aaa5dcdd413f641755247441e

rpm-ostree: Go back to working update check code
Commits 9c99472 and 9df8f0a regressed rpm-ostree update checking. This
needs more work, but for now go back to how we did it in 3.28 to get
back to a known working state for this release.

plugins/rpm-ostree/gs-plugin-rpm-ostree.c
@@ -425,58 +425,6 @@ make_rpmostree_options_variant (gboolean reboot,
 	return g_variant_ref_sink (g_variant_dict_end (&dict));
 }
 
-static gboolean
-_download_only (GsPlugin *plugin, GCancellable *cancellable, GError **error)
-{
-	GsPluginData *priv = gs_plugin_get_data (plugin);
-	g_autofree gchar *transaction_address = NULL;
-	g_autoptr(GVariant) options = NULL;
-
-	options = make_rpmostree_options_variant (FALSE,  /* reboot */
-						  FALSE,  /* allow-downgrade */
-						  FALSE,  /* cache-only */
-						  TRUE,   /* download-only */
-						  FALSE,  /* skip-purge */
-						  FALSE,  /* no-pull-base */
-						  FALSE,  /* dry-run */
-						  FALSE); /* no-overrides */
-	if (!gs_rpmostree_os_call_upgrade_sync (priv->os_proxy,
-						options,
-						NULL /* fd list */,
-						&transaction_address,
-						NULL /* fd list out */,
-						cancellable,
-						error)) {
-		gs_utils_error_convert_gio (error);
-		return FALSE;
-	}
-
-	if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
-							 transaction_address,
-							 cancellable,
-							 error)) {
-		gs_utils_error_convert_gio (error);
-		return FALSE;
-	}
-
-	return TRUE;
-}
-
-gboolean
-gs_plugin_download (GsPlugin *plugin,
-		    GsAppList *list,
-		    GCancellable *cancellable,
-		    GError **error)
-{
-	/* any are us? */
-	for (guint i = 0; i < gs_app_list_length (list); i++) {
-		GsApp *app = gs_app_list_index (list, i);
-		if (g_strcmp0 (gs_app_get_management_plugin (app), "rpm-ostree") == 0)
-			return _download_only (plugin, cancellable, error);
-	}
-	return TRUE;
-}
-
 gboolean
 gs_plugin_refresh (GsPlugin *plugin,
                    guint cache_age,
@@ -484,30 +432,68 @@ gs_plugin_refresh (GsPlugin *plugin,
                    GError **error)
 {
 	GsPluginData *priv = gs_plugin_get_data (plugin);
-	GVariantDict dict;
-	g_autofree gchar *transaction_address = NULL;
-	g_autoptr(GVariant) options = NULL;
 
-	g_variant_dict_init (&dict, NULL);
-	g_variant_dict_insert (&dict, "mode", "s", "check");
-	options = g_variant_ref_sink (g_variant_dict_end (&dict));
+	if (cache_age == G_MAXUINT)
+		return TRUE;
 
-	if (!gs_rpmostree_os_call_automatic_update_trigger_sync (priv->os_proxy,
-								 options,
-								 NULL,
-								 &transaction_address,
-								 cancellable,
-								 error)) {
-		gs_utils_error_convert_gio (error);
-		return FALSE;
+	{
+		g_autofree gchar *transaction_address = NULL;
+		g_autoptr(GVariant) options = NULL;
+
+		options = make_rpmostree_options_variant (FALSE,  /* reboot */
+		                                          FALSE,  /* allow-downgrade */
+		                                          FALSE,  /* cache-only */
+		                                          TRUE,   /* download-only */
+		                                          FALSE,  /* skip-purge */
+		                                          FALSE,  /* no-pull-base */
+		                                          FALSE,  /* dry-run */
+		                                          FALSE); /* no-overrides */
+		if (!gs_rpmostree_os_call_upgrade_sync (priv->os_proxy,
+		                                        options,
+		                                        NULL /* fd list */,
+		                                        &transaction_address,
+		                                        NULL /* fd list out */,
+		                                        cancellable,
+		                                        error)) {
+			gs_utils_error_convert_gio (error);
+			return FALSE;
+		}
+
+		if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
+		                                                 transaction_address,
+		                                                 cancellable,
+		                                                 error)) {
+			gs_utils_error_convert_gio (error);
+			return FALSE;
+		}
 	}
 
-	if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
-							 transaction_address,
-							 cancellable,
-							 error)) {
-		gs_utils_error_convert_gio (error);
-		return FALSE;
+	{
+		g_autofree gchar *transaction_address = NULL;
+		g_autoptr(GVariant) options = NULL;
+		GVariantDict dict;
+
+		g_variant_dict_init (&dict, NULL);
+		g_variant_dict_insert (&dict, "mode", "s", "check");
+		options = g_variant_ref_sink (g_variant_dict_end (&dict));
+
+		if (!gs_rpmostree_os_call_automatic_update_trigger_sync (priv->os_proxy,
+		                                                         options,
+		                                                         NULL,
+		                                                         &transaction_address,
+		                                                         cancellable,
+		                                                         error)) {
+			gs_utils_error_convert_gio (error);
+			return FALSE;
+		}
+
+		if (!gs_rpmostree_transaction_get_response_sync (priv->sysroot_proxy,
+		                                                 transaction_address,
+		                                                 cancellable,
+		                                                 error)) {
+			gs_utils_error_convert_gio (error);
+			return FALSE;
+		}
 	}
 
 	/* update UI */