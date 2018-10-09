ad5f36ee154c259cf3c59c98f071bcc0fbe16b34
Kalev Lember <klember@redhat.com>
2018-10-09 12:47:04 +0200

    Update Fedora distro upgrades text
    ... as per aday's suggestions.

--- plugins/fedora-pkgdb-collections/gs-plugin-fedora-pkgdb-collections.c
+++ plugins/fedora-pkgdb-collections/gs-plugin-fedora-pkgdb-collections.c
@@ -305,7 +305,7 @@ _create_upgrade_from_info (GsPlugin *plugin, PkgdbItem *item)
 	gs_app_set_name (app, GS_APP_QUALITY_LOWEST, item->name);
 	gs_app_set_summary (app, GS_APP_QUALITY_LOWEST,
 			    /* TRANSLATORS: this is a title for Fedora distro upgrades */
-			    _("Upgrade your Fedora system to the latest features and improvements."));
+			    _("Upgrade for the latest features, performance and stability improvements."));
 	gs_app_set_version (app, app_version);
 	gs_app_set_size_installed (app, 1024 * 1024 * 1024); /* estimate */
 	gs_app_set_size_download (app, 256 * 1024 * 1024); /* estimate */
