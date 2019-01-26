ed6302c5d17358cfa833d18a00d57857336646d5
Kalev Lember <klember@redhat.com>
2019-01-26 18:58:24 +0100

    app row: Drop flatpak (Nightly) suffix
    We now coalesce same app IDs together when searching and instead have a
    source selection drop down in the details page to choose which one to
    install, which means that the app row Nightly suffix doesn't work to
    distinguish the apps any more.
    
    In addition, it's super confusing when the app has already added a
    Nightly prefix and we now add a suffix as well.
    
    https://gitlab.gnome.org/GNOME/gnome-software/issues/557

--- src/gs-app-row.c
+++ src/gs-app-row.c
@@ -328,16 +328,9 @@ gs_app_row_refresh (GsAppRow *app_row)
 	}
 
 	/* name */
-	if (g_strcmp0 (gs_app_get_branch (priv->app), "master") == 0) {
-		g_autofree gchar *name = NULL;
-		/* TRANSLATORS: not translated to match what flatpak does */
-		name = g_strdup_printf ("%s (Nightly)",
-					gs_app_get_name (priv->app));
-		gtk_label_set_label (GTK_LABEL (priv->name_label), name);
-	} else {
-		gtk_label_set_label (GTK_LABEL (priv->name_label),
-				     gs_app_get_name (priv->app));
-	}
+	gtk_label_set_label (GTK_LABEL (priv->name_label),
+	                     gs_app_get_name (priv->app));
+
 	if (priv->show_update) {
 		const gchar *version_current = NULL;
 		const gchar *version_update = NULL;
