2b0700852829b14f3ed3e5a38b6ff2ce0ada9e40
Veerasamy Sevagen <sevagenv@gmail.com>
2019-03-07 11:18:06 +0000

    src/gs-loading-page.c: Setting loading page to "Downloading" dialogue only.
    The switching between downloading and loading happens in a moment. I do not think anyone would take time to read it.
    
    Fixes #612

--- src/gs-loading-page.c
+++ src/gs-loading-page.c
@@ -60,13 +60,10 @@ gs_loading_page_status_changed_cb (GsPluginLoader *plugin_loader,
 			/* TRANSLATORS: initial start */
 			str = _("Software catalog is being downloaded");
 		}
-	} else {
-		/* TRANSLATORS: initial start */
-		str = _("Software catalog is being loaded");
 	}
 	else	{
-			/* TRANSLATORS: initial start */
-			str = _("Software catalog is being downloaded");
+				/* TRANSLATORS: initial start */
+				str = _("Software catalog is being downloaded");
 	}
 
 	/* update label */
