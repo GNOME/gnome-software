aa728037d3fc45ed175a2df89e6f10b30eedd79b
Philip Withnall <withnall@endlessm.com>
2019-03-12 18:16:20 +0000

    parental-controls: Add new plugin for restricting access to apps
    This is a new plugin which loads the user’s parental controls settings
    (comprising a maximum OARS rating for apps to show, and a blacklist of
    apps to not allow launching) and use them to filter which apps are
    shown.
    
    The OARS rating filters non-installed apps so the user can only install
    apps which are age-appropriate for them. The blacklist filters installed
    apps so that the user cannot run inappropriate apps installed for other
    users.
    
    These are implemented using the parental controls quirks. The UI already
    pays attention to these quirks, and hides various buttons.
    
    This adds an optional dependency on libmalcontent
    (https://gitlab.freedesktop.org/pwithnall/malcontent).
    
    It is part of a wider approach to implementing parental controls, which
    includes filtering in the `flatpak` command, in gnome-shell and in the
    control center.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

---
+++ plugins/parental-controls/gs-plugin-parental-controls.c
@@ -0,0 +1,232 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2018-2019 Endless Mobile
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#include <config.h>
+
+#include <glib/gi18n.h>
+#include <gnome-software.h>
+#include <libmalcontent/app-filter.h>
+#include <string.h>
+#include <math.h>
+
+/*
+ * SECTION:
+ * Adds the %GS_APP_QUIRK_PARENTAL_FILTER and
+ * %GS_APP_QUIRK_PARENTAL_NOT_LAUNCHABLE quirks to applications if they
+ * contravene the effective user’s current parental controls policy.
+ *
+ * Specifically, %GS_APP_QUIRK_PARENTAL_FILTER will be added if an app’s OARS
+ * rating is too extreme for the current parental controls OARS policy.
+ * %GS_APP_QUIRK_PARENTAL_NOT_LAUNCHABLE will be added if the app is listed on
+ * the current parental controls blacklist.
+ *
+ * Parental controls policy is loaded using libmalcontent.
+ *
+ * This plugin is ordered after flatpak and appstream as it uses OARS data from
+ * them.
+ *
+ * Limiting access to applications by not allowing them to be launched by
+ * gnome-software is only one part of a wider approach to parental controls.
+ * In order to guarantee users do not have access to applications they shouldn’t
+ * have access to, an LSM (such as AppArmor) needs to be used. That complements,
+ * rather than substitutes for, filtering in user visible UIs.
+ */
+
+struct GsPluginData {
+	MctAppFilter	*app_filter;  /* (atomic) (owned) (nullable) */
+};
+
+/* Convert an #MctAppFilterOarsValue to an #AsContentRatingValue. This is
+ * actually a trivial cast, since the types are defined the same; but throw in
+ * a static assertion to be sure. */
+static AsContentRatingValue
+convert_app_filter_oars_value (MctAppFilterOarsValue filter_value)
+{
+  G_STATIC_ASSERT (AS_CONTENT_RATING_VALUE_LAST == MCT_APP_FILTER_OARS_VALUE_INTENSE + 1);
+
+  return (AsContentRatingValue) filter_value;
+}
+
+/* Check whether the OARS rating for @app is as, or less, extreme than the
+ * user’s preferences in @app_filter. If so (i.e. if the app is suitable for
+ * this user to use), return %TRUE; otherwise return %FALSE.
+ *
+ * The #AsContentRating in @app may be %NULL if no OARS ratings are provided for
+ * the app. If so, we have to assume the most restrictive ratings.
+ *
+ * We don’t need to worry about updating the app list when the app filter value
+ * is changed, as changing it requires logging out and back in as an
+ * administrator. */
+static gboolean
+app_is_content_rating_appropriate (GsApp *app, MctAppFilter *app_filter)
+{
+	AsContentRating *rating = gs_app_get_content_rating (app);  /* (nullable) */
+	g_autofree const gchar **oars_sections = mct_app_filter_get_oars_sections (app_filter);
+
+	if (rating == NULL)
+		g_debug ("No OARS ratings provided for ‘%s’: assuming most extreme",
+		         gs_app_get_unique_id (app));
+
+	for (gsize i = 0; oars_sections[i] != NULL; i++) {
+		AsContentRatingValue rating_value;
+		MctAppFilterOarsValue filter_value;
+
+		filter_value = mct_app_filter_get_oars_value (app_filter, oars_sections[i]);
+
+		if (rating != NULL)
+			rating_value = as_content_rating_get_value (rating, oars_sections[i]);
+		else
+			rating_value = AS_CONTENT_RATING_VALUE_INTENSE;
+
+		if (rating_value == AS_CONTENT_RATING_VALUE_UNKNOWN ||
+		    filter_value == MCT_APP_FILTER_OARS_VALUE_UNKNOWN)
+			continue;
+		else if (convert_app_filter_oars_value (filter_value) < rating_value)
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+static gboolean
+app_is_parentally_blacklisted (GsApp *app, MctAppFilter *app_filter)
+{
+	const gchar *desktop_id;
+	g_autoptr(GAppInfo) appinfo = NULL;
+
+	desktop_id = gs_app_get_id (app);
+	if (desktop_id == NULL)
+		return FALSE;
+	appinfo = G_APP_INFO (gs_utils_get_desktop_app_info (desktop_id));
+	if (appinfo == NULL)
+		return FALSE;
+
+	return !mct_app_filter_is_appinfo_allowed (app_filter, appinfo);
+}
+
+static gboolean
+parental_filter_if_needed (GsPlugin *plugin, GsApp *app, MctAppFilter *app_filter)
+{
+	/* Note that both quirks can be set on an app at the same time, and they
+	 * have slightly different meanings. */
+	gboolean filtered = FALSE;
+
+	/* Check the OARS ratings to see if this app should be installable. */
+	if (!app_is_content_rating_appropriate (app, app_filter)) {
+		g_debug ("Filtering ‘%s’: app OARS rating is too extreme for this user",
+		         gs_app_get_unique_id (app));
+		gs_app_add_quirk (app, GS_APP_QUIRK_PARENTAL_FILTER);
+		filtered = TRUE;
+	} else {
+		gs_app_remove_quirk (app, GS_APP_QUIRK_PARENTAL_FILTER);
+	}
+
+	/* Check the app blacklist to see if this app should be launchable. */
+	if (app_is_parentally_blacklisted (app, app_filter)) {
+		g_debug ("Filtering ‘%s’: app is blacklisted for this user",
+		         gs_app_get_unique_id (app));
+		gs_app_add_quirk (app, GS_APP_QUIRK_PARENTAL_NOT_LAUNCHABLE);
+		filtered = TRUE;
+	} else {
+		gs_app_remove_quirk (app, GS_APP_QUIRK_PARENTAL_NOT_LAUNCHABLE);
+	}
+
+	return filtered;
+}
+
+static MctAppFilter *
+query_app_filter (GCancellable  *cancellable,
+                  GError       **error)
+{
+	return mct_get_app_filter (NULL, getuid (), TRUE, cancellable, error);
+}
+
+void
+gs_plugin_initialize (GsPlugin *plugin)
+{
+	GsPluginData *priv = gs_plugin_alloc_data (plugin, sizeof (GsPluginData));
+	g_autoptr(GError) local_error = NULL;
+
+	priv->app_filter = query_app_filter (NULL, &local_error);
+
+	if (local_error != NULL) {
+		g_warning ("Failed to load parental controls filter: %s",
+			   local_error->message);
+	}
+
+	/* need application IDs and content ratings */
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "appstream");
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "flatpak");
+
+	/* set name of MetaInfo file */
+	gs_plugin_set_appstream_id (plugin, "org.gnome.Software.Plugin.ParentalControls");
+}
+
+gboolean
+gs_plugin_refine_app (GsPlugin *plugin,
+		      GsApp *app,
+		      GsPluginRefineFlags flags,
+		      GCancellable *cancellable,
+		      GError **error)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+	MctAppFilter *app_filter;
+
+	/* not valid */
+	if (gs_app_get_kind (app) != AS_APP_KIND_DESKTOP)
+		return TRUE;
+	if (gs_app_get_id (app) == NULL)
+		return TRUE;
+
+	/* Filter by various parental filters. If the filter is %NULL it’s
+	 * because the plugin failed to start up (and will have emitted a
+	 * warning at the time).*/
+	app_filter = g_atomic_pointer_get (&priv->app_filter);
+	if (app_filter == NULL)
+		return TRUE;
+
+	if (parental_filter_if_needed (plugin, app, app_filter))
+		return TRUE;
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refresh (GsPlugin *plugin,
+		   guint cache_age,
+		   GCancellable *cancellable,
+		   GError **error)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+	g_autoptr(MctAppFilter) new_app_filter = NULL;
+	g_autoptr(MctAppFilter) old_app_filter = NULL;
+
+	/* Refresh the app filter. This blocks on a D-Bus request. */
+	new_app_filter = query_app_filter (cancellable, error);
+
+	/* On failure, keep the old app filter around since it might be more
+	 * useful than nothing. */
+	if (new_app_filter == NULL)
+		return FALSE;
+
+	do {
+		old_app_filter = g_atomic_pointer_get (&priv->app_filter);
+	} while (!g_atomic_pointer_compare_and_exchange (&priv->app_filter,
+							 old_app_filter,
+							 new_app_filter));
+	new_app_filter = NULL;  /* ownership transferred above */
+
+	return TRUE;
+}
+
+void
+gs_plugin_destroy (GsPlugin *plugin)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+
+	g_clear_pointer (&priv->app_filter, mct_app_filter_unref);
+}
