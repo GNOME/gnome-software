27131c1cc0b5afff92cd66a2297f6d103b573488
Sundeep Anand <suanand@redhat.com>
2019-04-01 20:40:30 +0530

    implement gs_plugin_add_language_packs to install langpacks
    This can be used to add language packs to apps list as per active locale.
    And added language pack shall be installed by Update Monitor.

--- lib/gs-plugin-vfuncs.h
+++ lib/gs-plugin-vfuncs.h
@@ -961,4 +961,22 @@ gboolean	 gs_plugin_update			(GsPlugin	*plugin,
 							 GCancellable	*cancellable,
 							 GError		**error);
 
+/**
+ * gs_plugin_add_language_packs:
+ * @plugin: a #GsPlugin
+ * @list: a #GsAppList
+ * @language_code: a #LANGUAGE_CODE or #LOCALE, e.g. "ja" or "ja_JP"
+ * @cancellable: a #GCancellable, or %NULL
+ * @error: a #GError, or %NULL
+ *
+ * Returns a list of language packs, as per input language code or locale.
+ *
+ * Returns: %TRUE for success or if not relevant
+ **/
+gboolean	 gs_plugin_add_language_packs		(GsPlugin	*plugin,
+							 GsAppList	*list,
+							 const gchar    *language_code,
+							 GCancellable	*cancellable,
+							 GError		**error);
+
 G_END_DECLS
