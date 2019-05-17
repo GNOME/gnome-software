09d8ea700d79b4ac08f4757814924431ee3cc594
Sundeep Anand <suanand@redhat.com>
2019-05-17 22:00:46 +0530

    review comments

--- plugins/core/gs-plugin-fedora-langpacks.c
+++ plugins/core/gs-plugin-fedora-langpacks.c
@@ -19,6 +19,7 @@
 void
 gs_plugin_initialize (GsPlugin *plugin)
 {
+
   /* this plugin should be fedora specific */
   if (!gs_plugin_check_distro_id (plugin, "fedora")) {
     gs_plugin_set_enabled (plugin, FALSE);
@@ -39,34 +40,33 @@ gs_plugin_add_language_packs (GsPlugin *plugin,
   const gchar *identified_langpack_source;
   const gchar *lang_code_from_locale;
 
-  GsApp *app = NULL;
-  GHashTable* locale_langpack_map = g_hash_table_new(g_str_hash, g_str_equal);
+  GHashTable* locale_langpack_map = g_hash_table_new (g_str_hash, g_str_equal);
 
   /*
    * A few language code may have more than one language packs.
    * Example: en {en_GB}, pt {pt_BR}, zh {zh_CN, zh_TW}
    */
 
-  g_hash_table_insert(locale_langpack_map, "en_GB", "langpacks-en_GB");
-  g_hash_table_insert(locale_langpack_map, "pt_BR", "langpacks-pt_BR");
-  g_hash_table_insert(locale_langpack_map, "zh_CN", "langpacks-zh_CN");
-  g_hash_table_insert(locale_langpack_map, "zh_TW", "langpacks-zh_TW");
+  g_hash_table_insert (locale_langpack_map, "en_GB", "langpacks-en_GB");
+  g_hash_table_insert (locale_langpack_map, "pt_BR", "langpacks-pt_BR");
+  g_hash_table_insert (locale_langpack_map, "zh_CN", "langpacks-zh_CN");
+  g_hash_table_insert (locale_langpack_map, "zh_TW", "langpacks-zh_TW");
 
-  if (g_strrstr(language_code, "_") &&
+  if (g_strrstr(language_code, "_") != NULL &&
       !g_hash_table_lookup(locale_langpack_map, language_code)) {
 
     /* language_code is locale and not found in locale_langpack_map */
-    lang_code_from_locale = g_strsplit(language_code, "_", 2)[0];
+    lang_code_from_locale = g_strsplit (language_code, "_", 2)[0];
     identified_lang_id = lang_code_from_locale;
 
   } else {
     identified_lang_id = language_code;
   }
 
-  identified_langpack_source = g_strconcat("langpacks-", identified_lang_id, NULL);
+  identified_langpack_source = g_strconcat ("langpacks-", identified_lang_id, NULL);
   if (identified_langpack_source != NULL) {
-
-    app = gs_app_new (NULL);
+    
+    g_autoptr(GsApp) app = gs_app_new (NULL);
     gs_app_set_metadata (app, "GnomeSoftware::Creator", gs_plugin_get_name (plugin));
     gs_app_set_kind (app, AS_APP_KIND_LOCALIZATION);
     gs_app_add_source (app, identified_langpack_source);
