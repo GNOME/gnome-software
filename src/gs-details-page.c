d35f942f61c8014a9759178120c1d467fc392931
Philip Withnall <withnall@endlessm.com>
2019-11-12 21:18:09 +0000

    gs-details-page: Fix a minor leak in a comparison function
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- src/gs-details-page.c
+++ src/gs-details-page.c
@@ -2024,9 +2024,10 @@ origin_popover_list_sort_func (GtkListBoxRow *a,
 {
 	GsApp *a1 = gs_origin_popover_row_get_app (GS_ORIGIN_POPOVER_ROW (a));
 	GsApp *a2 = gs_origin_popover_row_get_app (GS_ORIGIN_POPOVER_ROW (b));
+	g_autofree gchar *a1_origin = gs_app_get_origin_ui (a1);
+	g_autofree gchar *a2_origin = gs_app_get_origin_ui (a2);
 
-	return g_strcmp0 (gs_app_get_origin_ui (a1),
-			  gs_app_get_origin_ui (a2));
+	return g_strcmp0 (a1_origin, a2_origin);
 }
 
 static void
