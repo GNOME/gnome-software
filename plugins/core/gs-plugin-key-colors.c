83bd3de5e9ce519031b914ad3d824841b7bc6279
Philip Withnall <withnall@endlessm.com>
2020-04-02 13:12:29 +0100

    key-colors: Pre-query pixbuf width and height
    Rather than querying them on every loop iteration. In a trace where I
    switched between categories a few times, `gdk_pixbuf_get_width()` was
    being avoidably called 61 million times.
    
    This introduces no functional changes.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- plugins/core/gs-plugin-key-colors.c
+++ plugins/core/gs-plugin-key-colors.c
@@ -58,7 +58,7 @@ static void
 gs_plugin_key_colors_set_for_pixbuf (GsApp *app, GdkPixbuf *pb, guint number)
 {
 	gint rowstride, n_channels;
-	gint x, y;
+	gint x, y, width, height;
 	guchar *pixels, *p;
 	guint bin_size = 200;
 	guint i;
@@ -68,12 +68,15 @@ gs_plugin_key_colors_set_for_pixbuf (GsApp *app, GdkPixbuf *pb, guint number)
 	n_channels = gdk_pixbuf_get_n_channels (pb);
 	rowstride = gdk_pixbuf_get_rowstride (pb);
 	pixels = gdk_pixbuf_get_pixels (pb);
+	width = gdk_pixbuf_get_width (pb);
+	height = gdk_pixbuf_get_height (pb);
+
 	for (bin_size = 250; bin_size > 0; bin_size -= 2) {
 		g_autoptr(GHashTable) hash = NULL;
 		hash = g_hash_table_new_full (g_direct_hash,  g_direct_equal,
 					      NULL, g_free);
-		for (y = 0; y < gdk_pixbuf_get_height (pb); y++) {
-			for (x = 0; x < gdk_pixbuf_get_width (pb); x++) {
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width; x++) {
 				CdColorRGB8 tmp;
 				GsColorBin *s;
 				gpointer key;
