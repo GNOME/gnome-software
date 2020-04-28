83b6768214f6c935baf8d6f4341134e5ca403013
Philip Withnall <withnall@endlessm.com>
2020-04-28 18:30:08 +0100

    gs-loading-page: Port to the new unknown progress support
    It already supported unknown progress, by assuming that 0% progress meant
    the progress was unknown. This commit formalises that a bit more.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    Helps: #276

--- src/gs-loading-page.c
+++ src/gs-loading-page.c
@@ -74,7 +74,8 @@ gs_loading_page_status_changed_cb (GsPluginLoader *plugin_loader,
 			g_source_remove (priv->progress_pulse_id);
 			priv->progress_pulse_id = 0;
 		}
-		if (gs_app_get_progress (app) == 0) {
+
+		if (gs_app_get_progress (app) == GS_APP_PROGRESS_UNKNOWN) {
 			priv->progress_pulse_id = g_timeout_add (50, _pulse_cb, self);
 		} else {
 			gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (priv->progressbar),
