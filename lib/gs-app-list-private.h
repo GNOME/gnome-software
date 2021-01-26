9f1d96208d217f83e7ef7b869e9bd14bc06b260a
Philip Withnall <pwithnall@endlessos.org>
2021-01-26 16:34:44 +0000

    lib: Move GsAppListFilterFlags definition to public header
    It’s used in the public `GsPluginJob:dedupe-flags` property, so should
    be public already.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- lib/gs-app-list-private.h
+++ lib/gs-app-list-private.h
@@ -34,33 +34,6 @@ typedef enum {
 	GS_APP_LIST_FLAG_LAST
 } GsAppListFlags;
 
-/**
- * GsAppListFilterFlags:
- * @GS_APP_LIST_FILTER_FLAG_NONE:		No flags set
- * @GS_APP_LIST_FILTER_FLAG_KEY_ID:		Filter by ID
- * @GS_APP_LIST_FILTER_FLAG_KEY_SOURCE:		Filter by default source
- * @GS_APP_LIST_FILTER_FLAG_KEY_VERSION:	Filter by version
- * @GS_APP_LIST_FILTER_FLAG_PREFER_INSTALLED:	Prefer installed applications
- * @GS_APP_LIST_FILTER_FLAG_KEY_ID_PROVIDES:	Filter using the provides ID
- *
- * Flags to use when filtering. The priority of each #GsApp is used to choose
- * which application object to keep.
- **/
-typedef enum {
-	GS_APP_LIST_FILTER_FLAG_NONE		= 0,
-	GS_APP_LIST_FILTER_FLAG_KEY_ID		= 1 << 0,
-	GS_APP_LIST_FILTER_FLAG_KEY_SOURCE	= 1 << 1,
-	GS_APP_LIST_FILTER_FLAG_KEY_VERSION	= 1 << 2,
-	GS_APP_LIST_FILTER_FLAG_PREFER_INSTALLED= 1 << 3,
-	GS_APP_LIST_FILTER_FLAG_KEY_ID_PROVIDES	= 1 << 4,
-	/*< private >*/
-	GS_APP_LIST_FILTER_FLAG_LAST,
-	GS_APP_LIST_FILTER_FLAG_MASK		= G_MAXUINT64
-} GsAppListFilterFlags;
-
-/* All the properties which use #GsAppListFilterFlags are guint64s. */
-G_STATIC_ASSERT (sizeof (GsAppListFilterFlags) == sizeof (guint64));
-
 GsAppList	*gs_app_list_copy		(GsAppList	*list);
 guint		 gs_app_list_get_size_peak	(GsAppList	*list);
 void		 gs_app_list_filter_duplicates	(GsAppList	*list,
