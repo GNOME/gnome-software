f2a7c88bef0a5977748b75a53dc6052b88d78419
Philip Withnall <pwithnall@endlessos.org>
2021-02-17 20:24:07 +0000

    gs-appstream: Load 256px icon for use by featured apps when in hi-DPI
    This could be optimised in future to only load the icon on demand, but
    for now this will get the featured apps carousel working in time for the
    UI freeze.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    Helps: #1131

--- plugins/core/gs-appstream.c
+++ plugins/core/gs-appstream.c
@@ -208,24 +208,20 @@ gs_appstream_refine_icon (GsPlugin *plugin, GsApp *app, XbNode *component)
 		gs_app_add_icon (app, icon);
 	}
 
-	/* if HiDPI get a 128px cached icon */
-	if (gs_plugin_get_scale (plugin) == 2) {
-		icon = gs_appstream_get_icon_by_kind_and_size (component,
-							       AS_ICON_KIND_CACHED,
-							       128);
-		if (icon != NULL) {
-			gs_app_add_icon (app, icon);
-			return;
-		}
-	}
-
-	/* non-HiDPI cached icon */
+	/* cached icon for large uses */
 	icon = gs_appstream_get_icon_by_kind_and_size (component,
 						       AS_ICON_KIND_CACHED,
-						       64);
+						       128 * gs_plugin_get_scale (plugin));
 	if (icon != NULL) {
 		gs_app_add_icon (app, icon);
-		return;
+	}
+
+	/* cached icon for normal uses */
+	icon = gs_appstream_get_icon_by_kind_and_size (component,
+						       AS_ICON_KIND_CACHED,
+						       64 * gs_plugin_get_scale (plugin));
+	if (icon != NULL) {
+		gs_app_add_icon (app, icon);
 	}
 
 	/* prefer local */
