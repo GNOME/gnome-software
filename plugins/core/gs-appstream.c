GNOME/gnome-software

commit f2a7c88bef0a5977748b75a53dc6052b88d78419

gs-appstream: Load 256px icon for use by featured apps when in hi-DPI
This could be optimised in future to only load the icon on demand, but
for now this will get the featured apps carousel working in time for the
UI freeze.

Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

Helps: #1131

plugins/core/gs-appstream.c
@@ -208,24 +208,20 @@ gs_appstream_refine_icon (GsPlugin *plugin, GsApp *app, XbNode *component)
 		gs_app_add_icon (app, icon);
 	}
 
-	/* if HiDPI get a 128px cached icon */
-	if (gs_plugin_get_scale (plugin) == 2) {
-		icon = gs_appstream_get_icon_by_kind_and_size (component,
-							       AS_ICON_KIND_CACHED,
-							       128);
-		if (icon != NULL) {
-			gs_app_add_icon (app, icon);
-			return;
-		}
-	}
-
-	/* non-HiDPI cached icon */
+	/* cached icon for large uses */
 	icon = gs_appstream_get_icon_by_kind_and_size (component,
 						       AS_ICON_KIND_CACHED,
-						       64);
+						       128 * gs_plugin_get_scale (plugin));
 	if (icon != NULL) {
 		gs_app_add_icon (app, icon);
-		return;
+	}
+
+	/* cached icon for normal uses */
+	icon = gs_appstream_get_icon_by_kind_and_size (component,
+						       AS_ICON_KIND_CACHED,
+						       64 * gs_plugin_get_scale (plugin));
+	if (icon != NULL) {
+		gs_app_add_icon (app, icon);
 	}
 
 	/* prefer local */

commit 296fb0543cd2d69944a49114eac2bfa56410a1e3

appstream: Fix some icon leaks in gs-appstream.c
These were accidentally introduced in commit f2a7c88bef.

Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

plugins/core/gs-appstream.c
@@ -214,6 +214,7 @@ gs_appstream_refine_icon (GsPlugin *plugin, GsApp *app, XbNode *component)
 		 * to style all apps in the software center). Since we can not rely on the icon's presence,
 		 * we also add other icons to the list and do not return here. */
 		app_add_icon (app, icon);
+		g_clear_object (&icon);
 	}
 
 	/* cached icon for large uses */
@@ -222,6 +223,7 @@ gs_appstream_refine_icon (GsPlugin *plugin, GsApp *app, XbNode *component)
 						       128 * gs_plugin_get_scale (plugin));
 	if (icon != NULL) {
 		app_add_icon (app, icon);
+		g_clear_object (&icon);
 	}
 
 	/* cached icon for normal uses */
@@ -230,6 +232,7 @@ gs_appstream_refine_icon (GsPlugin *plugin, GsApp *app, XbNode *component)
 						       64 * gs_plugin_get_scale (plugin));
 	if (icon != NULL) {
 		app_add_icon (app, icon);
+		g_clear_object (&icon);
 	}
 
 	/* prefer local */
@@ -258,6 +261,7 @@ gs_appstream_refine_icon (GsPlugin *plugin, GsApp *app, XbNode *component)
 	if (n != NULL) {
 		icon = gs_appstream_new_icon (component, n, AS_ICON_KIND_STOCK, 0);
 		app_add_icon (app, icon);
+		g_clear_object (&icon);
 	}
 }
 