22d6ffb0f0ce5c50c69a86ff2935bdf9a2821bad
Philip Withnall <pwithnall@endlessos.org>
2021-04-21 14:46:48 +0100

    gs-app-list: Pass the correct user_data to sort callbacks
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- lib/gs-app-list.c
+++ lib/gs-app-list.c
@@ -589,7 +589,7 @@ gs_app_list_sort_cb (gconstpointer a, gconstpointer b, gpointer user_data)
 	GsApp *app1 = GS_APP (*(GsApp **) a);
 	GsApp *app2 = GS_APP (*(GsApp **) b);
 	GsAppListSortHelper *helper = (GsAppListSortHelper *) user_data;
-	return helper->func (app1, app2, user_data);
+	return helper->func (app1, app2, helper->user_data);
 }
 
 /**
