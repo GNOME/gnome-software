GNOME/gnome-software

commit 15d39630bb74bd9f6e813bcbdd85b7173f3b7df0

core: Only define refine(), not refine_app(), where possible
This combines a lot of `gs_plugin_loader_call_vfunc()` calls when
refining a non-trivial list of apps; and it sometimes allows common
setup code (especially locking) to be shared between all apps in the
list, rather than being repeated for each `refine()_app` call.

This should improve performance slightly, depending on the size of app
lists being handled.

Signed-off-by: Philip Withnall <withnall@endlessm.com>

plugins/core/gs-plugin-appstream.c
@@ -786,29 +786,33 @@ gs_plugin_refine_from_pkgname (GsPlugin *plugin,
 }
 
 gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+gs_plugin_refine (GsPlugin *plugin,
+		  GsAppList *list,
+		  GsPluginRefineFlags flags,
+		  GCancellable *cancellable,
+		  GError **error)
 {
 	gboolean found = FALSE;
-
-	/* not us */
-	if (gs_app_get_bundle_kind (app) != AS_BUNDLE_KIND_PACKAGE &&
-	    gs_app_get_bundle_kind (app) != AS_BUNDLE_KIND_UNKNOWN)
-		return TRUE;
 
 	/* check silo is valid */
 	if (!gs_plugin_appstream_check_silo (plugin, cancellable, error))
 		return FALSE;
 
-	/* find by ID then fall back to package name */
-	if (!gs_plugin_refine_from_id (plugin, app, flags, &found, error))
-		return FALSE;
-	if (!found) {
-		if (!gs_plugin_refine_from_pkgname (plugin, app, flags, error))
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+
+		/* not us */
+		if (gs_app_get_bundle_kind (app) != AS_BUNDLE_KIND_PACKAGE &&
+		    gs_app_get_bundle_kind (app) != AS_BUNDLE_KIND_UNKNOWN)
+			return TRUE;
+
+		/* find by ID then fall back to package name */
+		if (!gs_plugin_refine_from_id (plugin, app, flags, &found, error))
 			return FALSE;
+		if (!found) {
+			if (!gs_plugin_refine_from_pkgname (plugin, app, flags, error))
+				return FALSE;
+		}
 	}
 
 	/* success */

plugins/core/gs-plugin-desktop-menu-path.c
@@ -40,12 +40,12 @@ _gs_app_has_desktop_group (GsApp *app, const gchar *desktop_group)
 }
 
 /* adds the menu-path for applications */
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	const gchar *strv[] = { "", NULL, NULL };
 	const GsDesktopData *msdata;
@@ -86,5 +86,25 @@ gs_plugin_refine_app (GsPlugin *plugin,
 
 	/* always set something to avoid keep searching for this */
 	gs_app_set_menu_path (app, (gchar **) strv);
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	/* nothing to do here */
+	if ((flags & GS_PLUGIN_REFINE_FLAGS_REQUIRE_MENU_PATH) == 0)
+		return TRUE;
+
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
+	}
+
 	return TRUE;
 }

plugins/core/gs-plugin-hardcoded-blacklist.c
@@ -22,12 +22,12 @@ gs_plugin_initialize (GsPlugin *plugin)
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "appstream");
 }
 
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	guint i;
 	const gchar *app_globs[] = {
@@ -55,6 +55,22 @@ gs_plugin_refine_app (GsPlugin *plugin,
 			gs_app_add_quirk (app, GS_APP_QUIRK_HIDE_EVERYWHERE);
 			break;
 		}
+	}
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
 	}
 
 	return TRUE;

plugins/core/gs-plugin-icons.c
@@ -252,12 +252,12 @@ gs_plugin_icons_load_cached (GsPlugin *plugin, AsIcon *icon, GError **error)
 	return g_object_ref (as_icon_get_pixbuf (icon));
 }
 
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	GPtrArray *icons;
 	guint i;
@@ -308,6 +308,26 @@ gs_plugin_refine_app (GsPlugin *plugin,
 		g_debug ("failed to load icon for %s: %s",
 			 gs_app_get_id (app),
 			 error_local->message);
+	}
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	/* nothing to do here */
+	if ((flags & GS_PLUGIN_REFINE_FLAGS_REQUIRE_ICON) == 0)
+		return TRUE;
+
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
 	}
 
 	return TRUE;

plugins/core/gs-plugin-key-colors-metadata.c
@@ -15,12 +15,12 @@ gs_plugin_initialize (GsPlugin *plugin)
 	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "key-colors");
 }
 
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	GPtrArray *key_colors;
 	const gchar *keys[] = {
@@ -66,6 +66,22 @@ gs_plugin_refine_app (GsPlugin *plugin,
 			gs_app_set_metadata (app, keys[i], css_new->str);
 		}
 
+	}
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
 	}
 
 	return TRUE;

plugins/core/gs-plugin-key-colors.c
@@ -142,12 +142,12 @@ gs_plugin_key_colors_set_for_pixbuf (GsApp *app, GdkPixbuf *pb, guint number)
 	}
 }
 
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	GdkPixbuf *pb;
 	g_autoptr(GdkPixbuf) pb_small = NULL;
@@ -170,5 +170,25 @@ gs_plugin_refine_app (GsPlugin *plugin,
 	/* get a list of key colors */
 	pb_small = gdk_pixbuf_scale_simple (pb, 32, 32, GDK_INTERP_BILINEAR);
 	gs_plugin_key_colors_set_for_pixbuf (app, pb_small, 10);
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	/* nothing to do here */
+	if ((flags & GS_PLUGIN_REFINE_FLAGS_REQUIRE_KEY_COLORS) == 0)
+		return TRUE;
+
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
+	}
+
 	return TRUE;
 }

plugins/core/gs-plugin-provenance-license.c
@@ -95,12 +95,12 @@ gs_plugin_destroy (GsPlugin *plugin)
 	g_object_unref (priv->settings);
 }
 
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	GsPluginData *priv = gs_plugin_get_data (plugin);
 	const gchar *origin;
@@ -121,6 +121,31 @@ gs_plugin_refine_app (GsPlugin *plugin,
 	origin = gs_app_get_origin (app);
 	if (origin != NULL && gs_utils_strv_fnmatch (priv->sources, origin))
 		gs_app_set_license (app, GS_APP_QUALITY_NORMAL, priv->license_id);
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+
+	/* nothing to do here */
+	if ((flags & GS_PLUGIN_REFINE_FLAGS_REQUIRE_LICENSE) == 0)
+		return TRUE;
+	/* nothing to search */
+	if (priv->sources == NULL || priv->sources[0] == NULL)
+		return TRUE;
+
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
+	}
 
 	return TRUE;
 }

plugins/core/gs-plugin-provenance.c
@@ -69,12 +69,12 @@ gs_plugin_destroy (GsPlugin *plugin)
 	g_object_unref (priv->settings);
 }
 
-gboolean
-gs_plugin_refine_app (GsPlugin *plugin,
-		      GsApp *app,
-		      GsPluginRefineFlags flags,
-		      GCancellable *cancellable,
-		      GError **error)
+static gboolean
+refine_app (GsPlugin             *plugin,
+	    GsApp                *app,
+	    GsPluginRefineFlags   flags,
+	    GCancellable         *cancellable,
+	    GError              **error)
 {
 	GsPluginData *priv = gs_plugin_get_data (plugin);
 	const gchar *origin;
@@ -111,5 +111,30 @@ gs_plugin_refine_app (GsPlugin *plugin,
 		gs_app_add_quirk (app, GS_APP_QUIRK_PROVENANCE);
 		return TRUE;
 	}
+	return TRUE;
+}
+
+gboolean
+gs_plugin_refine (GsPlugin             *plugin,
+		  GsAppList            *list,
+		  GsPluginRefineFlags   flags,
+		  GCancellable         *cancellable,
+		  GError              **error)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+
+	/* nothing to do here */
+	if ((flags & GS_PLUGIN_REFINE_FLAGS_REQUIRE_PROVENANCE) == 0)
+		return TRUE;
+	/* nothing to search */
+	if (priv->sources == NULL || priv->sources[0] == NULL)
+		return TRUE;
+
+	for (guint i = 0; i < gs_app_list_length (list); i++) {
+		GsApp *app = gs_app_list_index (list, i);
+		if (!refine_app (plugin, app, flags, cancellable, error))
+			return FALSE;
+	}
+
 	return TRUE;
 }

commit 13c6cf49990ada9e03a3494ba55187b042aa165b

gs-plugin-appstream: Correct gs_plugin_refine()
This is a regression from commit 15d39630b, the 'for' cycle should not return
from the function when it reaches an application the plugin cannot handle,
it should just continue with the next application in the list.

plugins/core/gs-plugin-appstream.c
@@ -908,7 +908,7 @@ gs_plugin_refine (GsPlugin *plugin,
 		/* not us */
 		if (gs_app_get_bundle_kind (app) != AS_BUNDLE_KIND_PACKAGE &&
 		    gs_app_get_bundle_kind (app) != AS_BUNDLE_KIND_UNKNOWN)
-			return TRUE;
+			continue;
 
 		/* find by ID then fall back to package name */
 		if (!gs_plugin_refine_from_id (plugin, app, flags, &found, error))