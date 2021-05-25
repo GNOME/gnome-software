69df42088fc8c327fbdd8fa8d244f684e3fa99f8
Milan Crha <mcrha@redhat.com>
2021-05-25 12:29:42 +0200

    Reviews: Ratings are rounded to integer stars
    Introduce and use a new GsStarImage widget, which can show partial ratings.
    
    Closes https://gitlab.gnome.org/GNOME/gnome-software/-/issues/1205

---
+++ src/gs-star-image.h
@@ -0,0 +1,22 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ * vi:set noexpandtab tabstop=8 shiftwidth=8:
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GS_TYPE_STAR_IMAGE		(gs_star_image_get_type ())
+
+G_DECLARE_FINAL_TYPE (GsStarImage, gs_star_image, GS, STAR_IMAGE, GtkWidget)
+
+GtkWidget *	gs_star_image_new		(void);
+void		gs_star_image_set_fraction	(GsStarImage *self,
+						 gdouble fraction);
+gdouble		gs_star_image_get_fraction	(GsStarImage *self);
+
+G_END_DECLS
