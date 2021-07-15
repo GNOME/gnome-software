304cdc1e44d468be23c49d881948471b278dfb15
Philip Withnall <pwithnall@endlessos.org>
2021-07-15 16:33:28 +0100

    gs-hardware-support-context-dialog: Add hardware support context dialogue
    This presents information about what hardware the app supports/requires,
    to the user.
    
    A future commit will make it appear when the hardware tile in
    `GsAppContextBar` is clicked.
    
    Some of the code for working out the hardware support is copied from the
    `GsAppContextBar`. It will be refactored to remove the duplication in a
    future commit.
    
    Includes significant work by Adrien Plazas.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    
    Helps: #1111

---
+++ src/gs-hardware-support-context-dialog.h
@@ -0,0 +1,50 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ * vi:set noexpandtab tabstop=8 shiftwidth=8:
+ *
+ * Copyright (C) 2021 Endless OS Foundation LLC
+ *
+ * Author: Philip Withnall <pwithnall@endlessos.org>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#pragma once
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "gs-app.h"
+
+G_BEGIN_DECLS
+
+#define GS_TYPE_HARDWARE_SUPPORT_CONTEXT_DIALOG (gs_hardware_support_context_dialog_get_type ())
+
+G_DECLARE_FINAL_TYPE (GsHardwareSupportContextDialog, gs_hardware_support_context_dialog, GS, HARDWARE_SUPPORT_CONTEXT_DIALOG, HdyWindow)
+
+GsHardwareSupportContextDialog	*gs_hardware_support_context_dialog_new		(GsApp				*app);
+
+GsApp				*gs_hardware_support_context_dialog_get_app	(GsHardwareSupportContextDialog	*self);
+void				 gs_hardware_support_context_dialog_set_app	(GsHardwareSupportContextDialog	*self,
+										 GsApp				*app);
+
+void gs_hardware_support_context_dialog_get_control_support (GdkDisplay     *display,
+                                                             GPtrArray      *relations,
+                                                             gboolean       *any_control_relations_set_out,
+                                                             AsRelationKind *control_relations,
+                                                             gboolean       *has_touchscreen_out,
+                                                             gboolean       *has_keyboard_out,
+                                                             gboolean       *has_mouse_out);
+
+GdkMonitor *gs_hardware_support_context_dialog_get_largest_monitor (GdkDisplay *display);
+void gs_hardware_support_context_dialog_get_display_support (GdkMonitor     *monitor,
+                                                             GPtrArray      *relations,
+                                                             gboolean       *any_display_relations_set_out,
+                                                             gboolean       *desktop_match_out,
+                                                             AsRelationKind *desktop_relation_kind_out,
+                                                             gboolean       *mobile_match_out,
+                                                             AsRelationKind *mobile_relation_kind_out,
+                                                             gboolean       *other_match_out,
+                                                             AsRelationKind *other_relation_kind_out);
+
+G_END_DECLS
