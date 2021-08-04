eb92c8ee10fc5333fca87ecfa0533a4e52d76b9b
Adrien Plazas <kekun.plazas@laposte.net>
2021-08-04 13:28:56 +0200

    Add GsOsUpdatePage
    This is extracted from GsUpdateDialog, which will use this widget
    instead in a later commit.

---
+++ src/gs-os-update-page.h
@@ -0,0 +1,26 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ * vi:set noexpandtab tabstop=8 shiftwidth=8:
+ *
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+#include "gnome-software-private.h"
+
+G_BEGIN_DECLS
+
+#define GS_TYPE_OS_UPDATE_PAGE (gs_os_update_page_get_type ())
+
+G_DECLARE_FINAL_TYPE (GsOsUpdatePage, gs_os_update_page, GS, OS_UPDATE_PAGE, GtkBox)
+
+GtkWidget	*gs_os_update_page_new		(void);
+GsApp		*gs_os_update_page_get_app	(GsOsUpdatePage	*page);
+void		 gs_os_update_page_set_app	(GsOsUpdatePage	*page,
+						 GsApp		*app);
+
+G_END_DECLS
