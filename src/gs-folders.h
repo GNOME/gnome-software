ea6642906c7153496f49974751a6930b41628d0b
Philip Withnall <pwithnall@endlessos.org>
2021-08-12 14:51:07 +0100

    gs-folders: Drop unused code
    Since commit 67a5bff938dad992a83b636b54c93f5d5d8a8415, this has been
    unused.
    
    This introduces no functional changes.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- src/gs-folders.h
+++ src/gs-folders.h
@@ -14,33 +14,6 @@
 
 G_BEGIN_DECLS
 
-#define GS_TYPE_FOLDERS (gs_folders_get_type ())
-
-G_DECLARE_FINAL_TYPE (GsFolders, gs_folders, GS, FOLDERS, GObject)
-
-GsFolders	 *gs_folders_get		(void);
-
-gchar		**gs_folders_get_folders	(GsFolders	*folders);
-gchar		**gs_folders_get_nonempty_folders (GsFolders	*folders);
-const gchar	 *gs_folders_add_folder		(GsFolders  	*folders,
-						 const gchar	*id);
-void		  gs_folders_remove_folder      (GsFolders      *folders,
-						 const gchar    *id);
-const gchar	 *gs_folders_get_folder_name	(GsFolders	*folders,
-						 const gchar    *id);
-void		  gs_folders_set_folder_name 	(GsFolders 	*folders,
-						 const gchar	*id,
-						 const gchar	*name);
-const gchar	 *gs_folders_get_app_folder	(GsFolders	*folders,
-						 const gchar	*app,
-						 GPtrArray      *categories);
-void		  gs_folders_set_app_folder	(GsFolders	*folders,
-						 const gchar	*app,
-						 GPtrArray      *categories,
-						 const gchar	*id);
-void		  gs_folders_save		(GsFolders 	*folders);
-void		  gs_folders_revert		(GsFolders	*folders);
-
 void              gs_folders_convert            (void);
 
 G_END_DECLS
