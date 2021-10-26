13d330dc3c215920938f58e4504d0d59ea90f523
Milan Crha <mcrha@redhat.com>
2021-10-26 08:44:59 +0200

    gs-update-dialog: Update window title with the current page title
    Make sure the window title is the same as the current page title.

--- src/gs-update-dialog.c
+++ src/gs-update-dialog.c
@@ -54,6 +54,15 @@ leaflet_child_transition_cb (AdwLeaflet *leaflet, GParamSpec *pspec, GsUpdateDia
 
 	while ((child = adw_leaflet_get_adjacent_child (leaflet, ADW_NAVIGATION_DIRECTION_FORWARD)))
 		adw_leaflet_remove (leaflet, child);
+
+	child = adw_leaflet_get_visible_child (leaflet);
+	if (child != NULL) {
+		g_autofree gchar *title = NULL;
+		g_object_get (G_OBJECT (child), "title", &title, NULL);
+		gtk_window_set_title (GTK_WINDOW (dialog), title);
+	} else {
+		gtk_window_set_title (GTK_WINDOW (dialog), "");
+	}
 }
 
 static void
