3b0452626630c9e0289463ecc256507cc8b9fae4
Milan Crha <mcrha@redhat.com>
2022-03-30 10:27:40 +0200

    gs-plugin-appstream: Use gs_appstream_load_desktop_files()
    To avoid code duplication.

--- plugins/core/gs-plugin-appstream.c
+++ plugins/core/gs-plugin-appstream.c
@@ -328,115 +328,6 @@ gs_plugin_appstream_load_appdata (GsPluginAppstream  *self,
 }
 
 static GInputStream *
-gs_plugin_appstream_load_desktop_cb (XbBuilderSource *self,
-				     XbBuilderSourceCtx *ctx,
-				     gpointer user_data,
-				     GCancellable *cancellable,
-				     GError **error)
-{
-	g_autofree gchar *xml = NULL;
-	g_autoptr(AsComponent) cpt = as_component_new ();
-	g_autoptr(AsContext) actx = as_context_new ();
-	g_autoptr(GBytes) bytes = NULL;
-	gboolean ret;
-
-	bytes = xb_builder_source_ctx_get_bytes (ctx, cancellable, error);
-	if (bytes == NULL)
-		return NULL;
-
-	as_component_set_id (cpt, xb_builder_source_ctx_get_filename (ctx));
-	ret = as_component_load_from_bytes (cpt,
-					   actx,
-					   AS_FORMAT_KIND_DESKTOP_ENTRY,
-					   bytes,
-					   error);
-	if (!ret)
-		return NULL;
-	xml = as_component_to_xml_data (cpt, actx, error);
-	if (xml == NULL)
-		return NULL;
-	return g_memory_input_stream_new_from_data (g_steal_pointer (&xml), (gssize) -1, g_free);
-}
-
-static gboolean
-gs_plugin_appstream_load_desktop_fn (GsPluginAppstream  *self,
-                                     XbBuilder          *builder,
-                                     const gchar        *filename,
-                                     GCancellable       *cancellable,
-                                     GError            **error)
-{
-	g_autoptr(GFile) file = g_file_new_for_path (filename);
-	g_autoptr(XbBuilderNode) info = NULL;
-	g_autoptr(XbBuilderSource) source = xb_builder_source_new ();
-
-	/* add support for desktop files */
-	xb_builder_source_add_adapter (source, "application/x-desktop",
-				       gs_plugin_appstream_load_desktop_cb, NULL, NULL);
-
-	/* add source */
-	if (!xb_builder_source_load_file (source, file, 0, cancellable, error))
-		return FALSE;
-
-	/* add metadata */
-	info = xb_builder_node_insert (NULL, "info", NULL);
-	xb_builder_node_insert_text (info, "filename", filename, NULL);
-	xb_builder_source_set_info (source, info);
-
-	/* success */
-	xb_builder_import_source (builder, source);
-	return TRUE;
-}
-
-static gboolean
-gs_plugin_appstream_load_desktop (GsPluginAppstream  *self,
-                                  XbBuilder          *builder,
-                                  const gchar        *path,
-                                  GCancellable       *cancellable,
-                                  GError            **error)
-{
-	const gchar *fn;
-	g_autoptr(GDir) dir = NULL;
-	g_autoptr(GFile) parent = g_file_new_for_path (path);
-	g_autoptr(GFileMonitor) file_monitor = NULL;
-	g_autoptr(GError) local_error = NULL;
-	if (!g_file_query_exists (parent, cancellable)) {
-		g_debug ("appstream: Skipping desktop path '%s' as %s", path, g_cancellable_is_cancelled (cancellable) ? "cancelled" : "does not exist");
-		return TRUE;
-	}
-
-	g_debug ("appstream: Loading desktop path '%s'", path);
-
-	dir = g_dir_open (path, 0, error);
-	if (dir == NULL)
-		return FALSE;
-
-	file_monitor = g_file_monitor (parent, G_FILE_MONITOR_NONE, cancellable, &local_error);
-	if (local_error)
-		g_debug ("appstream: Failed to create file monitor for '%s': %s", path, local_error->message);
-	gs_plugin_appstream_maybe_store_file_monitor (self, file_monitor);
-
-	while ((fn = g_dir_read_name (dir)) != NULL) {
-		if (g_str_has_suffix (fn, ".desktop")) {
-			g_autofree gchar *filename = g_build_filename (path, fn, NULL);
-			g_autoptr(GError) error_local = NULL;
-			if (g_strcmp0 (fn, "mimeinfo.cache") == 0)
-				continue;
-			if (!gs_plugin_appstream_load_desktop_fn (self,
-								  builder,
-								  filename,
-								  cancellable,
-								  &error_local)) {
-				g_debug ("ignoring %s: %s", filename, error_local->message);
-				continue;
-			}
-		}
-	}
-
-	/* success */
-	return TRUE;
-}
-
-static GInputStream *
 gs_plugin_appstream_load_dep11_cb (XbBuilderSource *self,
 				   XbBuilderSourceCtx *ctx,
 				   gpointer user_data,
@@ -852,17 +743,17 @@ gs_plugin_appstream_check_silo (GsPluginAppstream  *self,
 				return FALSE;
 			}
 		}
-		if (!gs_plugin_appstream_load_desktop (self, builder,
-						       DATADIR "/applications",
-						       cancellable, error)) {
+		if (!gs_appstream_load_desktop_files (builder,
+						      DATADIR "/applications",
+						      NULL, NULL, cancellable, error)) {
 			if (old_thread_default != NULL)
 				g_main_context_push_thread_default (old_thread_default);
 			return FALSE;
 		}
 		if (g_strcmp0 (DATADIR, "/usr/share") != 0 &&
-		    !gs_plugin_appstream_load_desktop (self, builder,
-						       "/usr/share/applications",
-						       cancellable, error)) {
+		    !gs_appstream_load_desktop_files (builder,
+						      "/usr/share/applications",
+						      NULL, NULL, cancellable, error)) {
 			if (old_thread_default != NULL)
 				g_main_context_push_thread_default (old_thread_default);
 			return FALSE;
