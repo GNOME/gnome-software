358436790fde39bba1cb1188067950c612ab5a07
Milan Crha <mcrha@redhat.com>
2022-03-31 11:57:31 +0200

    gs-appstream: Add gs_appstream_load_desktop_files()
    It can be used by the plugins, to avoid code duplication.

--- lib/gs-appstream.c
+++ lib/gs-appstream.c
@@ -2204,6 +2204,126 @@ gs_appstream_url_to_app (GsPlugin *plugin,
 	return TRUE;
 }
 
+static GInputStream *
+gs_appstream_load_desktop_cb (XbBuilderSource *self,
+			      XbBuilderSourceCtx *ctx,
+			      gpointer user_data,
+			      GCancellable *cancellable,
+			      GError **error)
+{
+	g_autofree gchar *xml = NULL;
+	g_autoptr(AsComponent) cpt = as_component_new ();
+	g_autoptr(AsContext) actx = as_context_new ();
+	g_autoptr(GBytes) bytes = NULL;
+	gboolean ret;
+
+	bytes = xb_builder_source_ctx_get_bytes (ctx, cancellable, error);
+	if (bytes == NULL)
+		return NULL;
+
+	as_component_set_id (cpt, xb_builder_source_ctx_get_filename (ctx));
+	ret = as_component_load_from_bytes (cpt,
+					   actx,
+					   AS_FORMAT_KIND_DESKTOP_ENTRY,
+					   bytes,
+					   error);
+	if (!ret)
+		return NULL;
+	xml = as_component_to_xml_data (cpt, actx, error);
+	if (xml == NULL)
+		return NULL;
+	return g_memory_input_stream_new_from_data (g_steal_pointer (&xml), (gssize) -1, g_free);
+}
+
+static gboolean
+gs_appstream_load_desktop_fn (XbBuilder     *builder,
+			      const gchar   *filename,
+			      GCancellable  *cancellable,
+			      GError        **error)
+{
+	g_autoptr(GFile) file = g_file_new_for_path (filename);
+	g_autoptr(XbBuilderNode) info = NULL;
+	g_autoptr(XbBuilderSource) source = xb_builder_source_new ();
+
+	/* add support for desktop files */
+	xb_builder_source_add_adapter (source, "application/x-desktop",
+				       gs_appstream_load_desktop_cb, NULL, NULL);
+
+	/* add source */
+	if (!xb_builder_source_load_file (source, file,
+#if LIBXMLB_CHECK_VERSION(0, 2, 0)
+					  XB_BUILDER_SOURCE_FLAG_WATCH_DIRECTORY,
+#else
+					  XB_BUILDER_SOURCE_FLAG_WATCH_FILE,
+#endif
+					  cancellable,
+					  error)) {
+		return FALSE;
+	}
+
+	/* add metadata */
+	info = xb_builder_node_insert (NULL, "info", NULL);
+	xb_builder_node_insert_text (info, "filename", filename, NULL);
+	xb_builder_source_set_info (source, info);
+
+	/* success */
+	xb_builder_import_source (builder, source);
+	return TRUE;
+}
+
+gboolean
+gs_appstream_load_desktop_files (XbBuilder      *builder,
+				 const gchar    *path,
+				 gboolean	*out_any_loaded,
+				 GFileMonitor  **out_file_monitor,
+				 GCancellable   *cancellable,
+				 GError         **error)
+{
+	const gchar *fn;
+	g_autoptr(GDir) dir = NULL;
+	g_autoptr(GFile) parent = g_file_new_for_path (path);
+	if (out_any_loaded)
+		*out_any_loaded = FALSE;
+	if (!g_file_query_exists (parent, cancellable)) {
+		g_debug ("appstream: Skipping desktop path '%s' as %s", path, g_cancellable_is_cancelled (cancellable) ? "cancelled" : "does not exist");
+		return TRUE;
+	}
+
+	g_debug ("appstream: Loading desktop path '%s'", path);
+
+	dir = g_dir_open (path, 0, error);
+	if (dir == NULL)
+		return FALSE;
+
+	if (out_file_monitor != NULL) {
+		g_autoptr(GError) error_local = NULL;
+		*out_file_monitor = g_file_monitor (parent, G_FILE_MONITOR_NONE, cancellable, &error_local);
+		if (error_local)
+			g_debug ("appstream: Failed to create file monitor for '%s': %s", path, error_local->message);
+	}
+
+	while ((fn = g_dir_read_name (dir)) != NULL) {
+		if (g_str_has_suffix (fn, ".desktop")) {
+			g_autofree gchar *filename = g_build_filename (path, fn, NULL);
+			g_autoptr(GError) error_local = NULL;
+			if (g_strcmp0 (fn, "mimeinfo.cache") == 0)
+				continue;
+			if (!gs_appstream_load_desktop_fn (builder,
+							   filename,
+							   cancellable,
+							   &error_local)) {
+				g_debug ("ignoring %s: %s", filename, error_local->message);
+				continue;
+			}
+			if (out_any_loaded)
+				*out_any_loaded = TRUE;
+		}
+	}
+
+	/* success */
+	return TRUE;
+}
+
 void
 gs_appstream_component_add_keyword (XbBuilderNode *component, const gchar *str)
 {
