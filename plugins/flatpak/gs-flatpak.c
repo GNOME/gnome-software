51aa7d28c76af05989aa852e434c4b0f396c96dc
Milan Crha <mcrha@redhat.com>
2022-06-20 21:03:21 +0200

    flatpak: Mute "libflatpak failed to return application size" warning
    It can happen when opening a .flatpakref file with filtered remote.

--- plugins/flatpak/gs-flatpak.c
+++ plugins/flatpak/gs-flatpak.c
@@ -2829,8 +2829,8 @@ gs_plugin_refine_item_size (GsFlatpak *self,
 								   &error_local);
 
 		if (!ret) {
-			g_warning ("libflatpak failed to return application "
-				   "size: %s", error_local->message);
+			/* This can happen when the remote is filtered */
+			g_debug ("libflatpak failed to return application size: %s", error_local->message);
 			g_clear_error (&error_local);
 		} else {
 			size_type = GS_SIZE_TYPE_VALID;
