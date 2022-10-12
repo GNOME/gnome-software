712362df2fb56c3bdbd25b2cb678bbd45c413638
Milan Crha <mcrha@redhat.com>
2022-10-12 12:28:12 +0200

    fwupd: Show hidden updates and provide a problem information for them
    An update can be hidden due to a problem the user can do something about it,
    like when the update cannot be applied, because a lid is closed. Check for
    these states and set them on the GsApp for further processing on the UI side.

--- plugins/fwupd/gs-fwupd-app.h
+++ plugins/fwupd/gs-fwupd-app.h
@@ -24,6 +24,7 @@ void			 gs_fwupd_app_set_update_uri		(GsApp		*app,
 void			 gs_fwupd_app_set_is_locked		(GsApp		*app,
 								 gboolean	 is_locked);
 void			 gs_fwupd_app_set_from_device		(GsApp		*app,
+								 FwupdClient	*client,
 								 FwupdDevice	*dev);
 void			 gs_fwupd_app_set_from_release		(GsApp		*app,
 								 FwupdRelease	*rel);
