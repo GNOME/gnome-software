8cca79b3ad18a8aa9770438ff041a6317148aec4
Milan Crha <mcrha@redhat.com>
2022-11-08 09:03:30 +0100

    gs-page: Always use non-NULL cancellable in gs_page_remove_app()
    When the caller passes no cancellable, use the one for the app, thus
    the operation can be cancelled.
    
    Closes https://gitlab.gnome.org/GNOME/gnome-software/-/issues/1830

--- src/gs-page.c
+++ src/gs-page.c
@@ -489,7 +489,7 @@ gs_page_remove_app (GsPage *page, GsApp *app, GCancellable *cancellable)
 	helper->action = GS_PLUGIN_ACTION_REMOVE;
 	helper->app = g_object_ref (app);
 	helper->page = g_object_ref (page);
-	helper->cancellable = cancellable != NULL ? g_object_ref (cancellable) : NULL;
+	helper->cancellable = g_object_ref (cancellable != NULL ? cancellable : gs_app_get_cancellable (app));
 	if (gs_app_get_state (app) == GS_APP_STATE_QUEUED_FOR_INSTALL) {
 		g_autoptr(GsPluginJob) plugin_job = NULL;
 
