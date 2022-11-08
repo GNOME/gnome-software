8bee26cc972df65a05333334bca43497c2ad2d74
Milan Crha <mcrha@redhat.com>
2022-11-08 08:56:36 +0100

    gs-plugin-loader: Do not remove app from install queue when in offline
    The plugin can left the app queued for install when there is no network
    available, in which case the app cannot be removed from the install queue.

--- lib/gs-plugin-loader.c
+++ lib/gs-plugin-loader.c
@@ -3302,16 +3302,22 @@ gs_plugin_loader_process_thread_cb (GTask *task,
 
 	/* remove from pending list */
 	if (add_to_pending_array) {
-		g_autoptr(GsAppList) addons = NULL;
+		GsApp *app = gs_plugin_job_get_app (helper->plugin_job);
+		/* The plugin can left the app queued for install when there is no network available,
+		   in which case the app cannot be removed from the install queue. */
+		if (action != GS_PLUGIN_ACTION_INSTALL ||
+		    gs_app_get_state (app) != GS_APP_STATE_QUEUED_FOR_INSTALL) {
+			g_autoptr(GsAppList) addons = NULL;
 
-		gs_plugin_loader_pending_apps_remove (plugin_loader, helper);
+			gs_plugin_loader_pending_apps_remove (plugin_loader, helper);
 
-		/* unstage addons */
-		addons = gs_app_dup_addons (gs_plugin_job_get_app (helper->plugin_job));
-		for (guint i = 0; addons != NULL && i < gs_app_list_length (addons); i++) {
-			GsApp *addon = gs_app_list_index (addons, i);
-			if (gs_app_get_to_be_installed (addon))
-				gs_app_set_to_be_installed (addon, FALSE);
+			/* unstage addons */
+			addons = gs_app_dup_addons (gs_plugin_job_get_app (helper->plugin_job));
+			for (guint i = 0; addons != NULL && i < gs_app_list_length (addons); i++) {
+				GsApp *addon = gs_app_list_index (addons, i);
+				if (gs_app_get_to_be_installed (addon))
+					gs_app_set_to_be_installed (addon, FALSE);
+			}
 		}
 	}
 
