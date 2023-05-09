GNOME/gnome-software

commit 550ad49a7d22403ed62c515db97d1df4b921ea0c

gs-app: Add thread safety around accessing GsApp::icons member
Some functions did lock a mutex while accessing the 'icons' member,
but some not. Furthermore, the public API returns an internal pointer,
which can be modified by other thread after being returned. The new
functions add thread safety to all accesses.

lib/gs-app.c
@@ -1946,6 +1946,7 @@ gs_app_get_icon_for_size (GsApp       *app,
                           const gchar *fallback_icon_name)
 {
 	GsAppPrivate *priv = gs_app_get_instance_private (app);
+	g_autoptr(GMutexLocker) locker = NULL;
 
 	g_return_val_if_fail (GS_IS_APP (app), NULL);
 	g_return_val_if_fail (size > 0, NULL);
@@ -1953,6 +1954,8 @@ gs_app_get_icon_for_size (GsApp       *app,
 
 	g_debug ("Looking for icon for %s, at size %u×%u, with fallback %s",
 		 gs_app_get_id (app), size, scale, fallback_icon_name);
+
+	locker = g_mutex_locker_new (&priv->mutex);
 
 	/* See if there’s an icon of the right size, or the first one which is too
 	 * big which could be scaled down. Note that the icons array may be
@@ -2045,17 +2048,82 @@ gs_app_get_action_screenshot (GsApp *app)
  *     or %NULL if there are no icons
  *
  * Since: 3.22
+ *
+ * Deprecated: 45: Use gs_app_dup_icons() or gs_app_has_icons() instead.
  **/
 GPtrArray *
 gs_app_get_icons (GsApp *app)
 {
 	GsAppPrivate *priv = gs_app_get_instance_private (app);
+	g_autoptr(GMutexLocker) locker = NULL;
+
 	g_return_val_if_fail (GS_IS_APP (app), NULL);
 
-	if (priv->icons != NULL && priv->icons->len == 0)
+	locker = g_mutex_locker_new (&priv->mutex);
+
+	if (priv->icons == NULL || priv->icons->len == 0)
 		return NULL;
 
 	return priv->icons;
+}
+
+/**
+ * gs_app_dup_icons:
+ * @app: a #GsApp
+ *
+ * Gets the icons for the application in a thread safe way.
+ *
+ * This will never return an empty array; it will always return either %NULL or
+ * a non-empty array.
+ *
+ * Returns: (transfer container) (element-type GIcon) (nullable): an array of icons,
+ *     or %NULL if there are no icons
+ *
+ * Since: 45
+ **/
+GPtrArray *
+gs_app_dup_icons (GsApp *app)
+{
+	GsAppPrivate *priv = gs_app_get_instance_private (app);
+	g_autoptr(GMutexLocker) locker = NULL;
+	GPtrArray *copy;
+
+	g_return_val_if_fail (GS_IS_APP (app), NULL);
+
+	locker = g_mutex_locker_new (&priv->mutex);
+
+	if (priv->icons == NULL || priv->icons->len == 0)
+		return NULL;
+
+	copy = g_ptr_array_new_full (priv->icons->len, g_object_unref);
+	for (guint i = 0; i < priv->icons->len; i++) {
+		g_ptr_array_add (copy, g_object_ref (g_ptr_array_index (priv->icons, i)));
+	}
+
+	return copy;
+}
+
+/**
+ * gs_app_has_icons:
+ * @app: a #GsApp
+ *
+ * Checks whether there are any icons set.
+ *
+ * Returns: %TRUE, when the @app has set any icons, %FALSE otherwise
+ *
+ * Since: 45
+ **/
+gboolean
+gs_app_has_icons (GsApp *app)
+{
+	GsAppPrivate *priv = gs_app_get_instance_private (app);
+	g_autoptr(GMutexLocker) locker = NULL;
+
+	g_return_val_if_fail (GS_IS_APP (app), FALSE);
+
+	locker = g_mutex_locker_new (&priv->mutex);
+
+	return priv->icons != NULL && priv->icons->len > 0;
 }
 
 static gint

lib/gs-app.h
@@ -385,7 +385,10 @@ GIcon		*gs_app_get_icon_for_size	(GsApp		*app,
 						 guint		 size,
 						 guint		 scale,
 						 const gchar	*fallback_icon_name);
+G_DEPRECATED_FOR(gs_app_dup_icons)
 GPtrArray	*gs_app_get_icons		(GsApp		*app);
+GPtrArray	*gs_app_dup_icons		(GsApp		*app);
+gboolean	 gs_app_has_icons		(GsApp		*app);
 void		 gs_app_add_icon		(GsApp		*app,
 						 GIcon		*icon);
 void		 gs_app_remove_all_icons	(GsApp		*app);

commit 43cc3411fedcf1c8102a3228730c77cd6e6833af

gs-app: Fix deadlock on recursive icon lookup
The function was recursing and trying to acquire the lock it already
held. Fixes a regression in commit 550ad49a7d.

Backtrace on deadlock:
```
2  0x00007ffff6962d97 in g_mutex_lock (mutex=0x617000036f00) at ../../source/glib/glib/gthread-posix.c:1417
3  0x00007ffff736aea9 in g_mutex_locker_new (mutex=0x617000036f00) at /opt/gnome/install/include/glib-2.0/glib/gthread.h:335
4  0x00007ffff737d3c2 in gs_app_get_icon_for_size (app=0x617000037170, size=128, scale=1, fallback_icon_name=0x0) at ../../source/gnome-software/lib/gs-app.c:1958
5  0x00007ffff737e0ad in gs_app_get_icon_for_size (app=0x617000037170, size=128, scale=2, fallback_icon_name=0x0) at ../../source/gnome-software/lib/gs-app.c:2010
6  0x00000000004f3b8b in filter_hi_res_icon (app=0x617000037170, user_data=0x61600008f000) at ../../source/gnome-software/src/gs-overview-page.c:288
```

Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

lib/gs-app.c
@@ -2005,6 +2005,8 @@ gs_app_get_icon_for_size (GsApp       *app,
 		}
 	}
 
+	g_clear_pointer (&locker, g_mutex_locker_free);
+
 	if (scale > 1) {
 		g_debug ("Retrying at scale 1");
 		return gs_app_get_icon_for_size (app, size, 1, fallback_icon_name);