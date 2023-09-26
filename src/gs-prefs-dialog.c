e72498ccc1e6c654885322da78bb490fc39fc90b
Milan Crha <mcrha@redhat.com>
2023-09-26 13:42:15 +0200

    gs-prefs-dialog: Update look of the Preferences dialog
    Follow design from https://gitlab.gnome.org/Teams/Design/software-mockups/-/issues/17
    
    Closes https://gitlab.gnome.org/GNOME/gnome-software/-/issues/2351

--- src/gs-prefs-dialog.c
+++ src/gs-prefs-dialog.c
@@ -23,10 +23,13 @@ struct _GsPrefsDialog
 
 	GCancellable		*cancellable;
 	GsPluginLoader		*plugin_loader;
-	GtkWidget		*switch_updates;
+	GtkWidget		*automatic_updates_radio;
+	GtkWidget		*manual_updates_radio;
 	GtkWidget		*switch_updates_notify;
 	GtkWidget		*switch_free_apps;
+	GtkPopover		*updates_info_popover;
 	AdwActionRow		*automatic_updates_row;
+	AdwActionRow		*manual_updates_row;
 	AdwActionRow		*automatic_update_notifications_row;
 	AdwActionRow		*show_only_free_apps_row;
 };
@@ -37,6 +40,18 @@ static void
 gs_prefs_dialog_show_only_free_apps_changed_cb (GsPrefsDialog *self)
 {
 	g_signal_emit_by_name (self->plugin_loader, "reload", 0);
+}
+
+static gboolean
+gs_prefs_dialog_automatic_updates_to_radio_cb (GValue *value,
+					       GVariant *variant,
+					       gpointer user_data)
+{
+	GsPrefsDialog *self = user_data;
+	if (!g_variant_get_boolean (variant))
+		gtk_check_button_set_active (GTK_CHECK_BUTTON (self->manual_updates_radio), TRUE);
+	g_value_set_boolean (value, g_variant_get_boolean (variant));
+	return TRUE;
 }
 
 static void
@@ -63,11 +78,13 @@ gs_prefs_dialog_init (GsPrefsDialog *dialog)
 			 dialog->switch_updates_notify,
 			 "active",
 			 G_SETTINGS_BIND_DEFAULT);
-	g_settings_bind (dialog->settings,
-			 "download-updates",
-			 dialog->switch_updates,
-			 "active",
-			 G_SETTINGS_BIND_DEFAULT);
+	g_settings_bind_with_mapping (dialog->settings,
+				      "download-updates",
+				      dialog->automatic_updates_radio,
+				      "active",
+				      G_SETTINGS_BIND_DEFAULT,
+				      gs_prefs_dialog_automatic_updates_to_radio_cb,
+				      NULL, dialog, NULL);
 	g_settings_bind (dialog->settings,
 			 "show-only-free-apps",
 			 dialog->switch_free_apps,
@@ -78,6 +95,7 @@ gs_prefs_dialog_init (GsPrefsDialog *dialog)
 
 #if ADW_CHECK_VERSION(1,2,0)
 	adw_preferences_row_set_use_markup (ADW_PREFERENCES_ROW (dialog->automatic_updates_row), FALSE);
+	adw_preferences_row_set_use_markup (ADW_PREFERENCES_ROW (dialog->manual_updates_row), FALSE);
 	adw_preferences_row_set_use_markup (ADW_PREFERENCES_ROW (dialog->automatic_update_notifications_row), FALSE);
 	adw_preferences_row_set_use_markup (ADW_PREFERENCES_ROW (dialog->show_only_free_apps_row), FALSE);
 #endif
@@ -92,10 +110,13 @@ gs_prefs_dialog_class_init (GsPrefsDialogClass *klass)
 	object_class->dispose = gs_prefs_dialog_dispose;
 
 	gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Software/gs-prefs-dialog.ui");
-	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, switch_updates);
+	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, automatic_updates_radio);
+	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, manual_updates_radio);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, switch_updates_notify);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, switch_free_apps);
+	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, updates_info_popover);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, automatic_updates_row);
+	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, manual_updates_row);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, automatic_update_notifications_row);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, show_only_free_apps_row);
 }
