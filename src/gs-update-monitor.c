94658a33a34d2b867db8300066f5045b71f55fa7
Milan Crha <mcrha@redhat.com>
2024-01-29 17:54:38 +0100

    gs-update-monitor: Replace GS_PLUGIN_ACTION_GET_UPDATES_HISTORICAL job with a list job

--- src/gs-update-monitor.c
+++ src/gs-update-monitor.c
@@ -1301,15 +1301,18 @@ static gboolean
 cleanup_notifications_cb (gpointer user_data)
 {
 	GsUpdateMonitor *monitor = user_data;
+	g_autoptr(GsAppQuery) query = NULL;
 	g_autoptr(GsPluginJob) plugin_job = NULL;
 
 	/* this doesn't do any network access, and is only called once just
 	 * after startup, so don’t cancel it with refreshes/updates */
 	g_debug ("getting historical updates for fresh session");
-	plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_GET_UPDATES_HISTORICAL,
-					 "refine-flags", GS_PLUGIN_REFINE_FLAGS_REQUIRE_VERSION,
-					 "propagate-error", TRUE,
-					 NULL);
+	query = gs_app_query_new ("is-updates-historical", GS_APP_QUERY_TRISTATE_TRUE,
+				  "refine-flags", GS_PLUGIN_REFINE_FLAGS_REQUIRE_VERSION |
+						  GS_PLUGIN_REFINE_FLAGS_DISABLE_FILTERING,
+				  NULL);
+	plugin_job = gs_plugin_job_list_apps_new (query, GS_PLUGIN_LIST_APPS_FLAGS_NONE);
+	gs_plugin_job_set_propagate_error (plugin_job, TRUE);
 	gs_plugin_loader_job_process_async (monitor->plugin_loader,
 					    plugin_job,
 					    monitor->shutdown_cancellable,
