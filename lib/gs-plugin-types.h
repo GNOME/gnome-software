c12cd65472c4656766e0b0d645aff7c93020b693
Milan Crha <mcrha@redhat.com>
2024-01-31 17:35:01 +0100

    gs-plugin: Add virtual functions for trigger-upgrade operation
    Helps: #1472

--- lib/gs-plugin-types.h
+++ lib/gs-plugin-types.h
@@ -333,6 +333,20 @@ typedef enum {
 } GsPluginDownloadUpgradeFlags;
 
 /**
+ * GsPluginTriggerUpgradeFlags:
+ * @GS_PLUGIN_TRIGGER_UPGRADE_FLAGS_NONE: No flags set.
+ * @GS_PLUGIN_TRIGGER_UPGRADE_FLAGS_INTERACTIVE: User initiated the job.
+ *
+ * Flags for an operation to trigger an upgrade.
+ *
+ * Since: 47
+ */
+typedef enum {
+	GS_PLUGIN_TRIGGER_UPGRADE_FLAGS_NONE		= 0,
+	GS_PLUGIN_TRIGGER_UPGRADE_FLAGS_INTERACTIVE	= 1 << 0,
+} GsPluginTriggerUpgradeFlags;
+
+/**
  * GsPluginProgressCallback:
  * @plugin: the #GsPlugin reporting its progress
  * @progress: the percentage completion (0–100 inclusive), or
--- lib/gs-plugin.h
+++ lib/gs-plugin.h
@@ -108,6 +108,11 @@ G_DECLARE_DERIVABLE_TYPE (GsPlugin, gs_plugin, GS, PLUGIN, GObject)
  * @download_upgrade_finish: (nullable): Finish method for
  *   @download_upgrade_async. Must be implemented if
  *   @download_upgrade_async is implemented. (Since: 47)
+ * @trigger_upgrade_async: (nullable): Triggers the distribution upgrade to be
+ *   installed on next boot. (Since: 47)
+ * @trigger_upgrade_finish: (nullable): Finish method for
+ *   @trigger_upgrade_async. Must be implemented if
+ *   @trigger_upgrade_async is implemented. (Since: 47)
  *
  * The class structure for a #GsPlugin. Virtual methods here should be
  * implemented by plugin implementations derived from #GsPlugin to provide their
@@ -298,6 +303,16 @@ struct _GsPluginClass
 								 GAsyncReadyCallback		 callback,
 								 gpointer			 user_data);
 	gboolean		(*download_upgrade_finish)	(GsPlugin			*plugin,
+								 GAsyncResult			*result,
+								 GError				**error);
+
+	void			(*trigger_upgrade_async)	(GsPlugin			*plugin,
+								 GsApp				*app,
+								 GsPluginTriggerUpgradeFlags	 flags,
+								 GCancellable			*cancellable,
+								 GAsyncReadyCallback		 callback,
+								 gpointer			 user_data);
+	gboolean		(*trigger_upgrade_finish)	(GsPlugin			*plugin,
 								 GAsyncResult			*result,
 								 GError				**error);
 
