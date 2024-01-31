4d58a57a51ec39dd411d7b4513c954dc7caf4ae4
Milan Crha <mcrha@redhat.com>
2024-01-31 17:48:45 +0100

    dummy: Replace GS_PLUGIN_ACTION_UPGRADE_TRIGGER job with trigger-upgrade job in tests
    Helps: #1472

--- plugins/dummy/gs-self-test.c
+++ plugins/dummy/gs-self-test.c
@@ -319,9 +319,7 @@ gs_plugins_dummy_distro_upgrades_func (GsPluginLoader *plugin_loader)
 
 	/* trigger the update */
 	g_object_unref (plugin_job);
-	plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_UPGRADE_TRIGGER,
-					 "app", app,
-					 NULL);
+	plugin_job = gs_plugin_job_trigger_upgrade_new (app, GS_PLUGIN_TRIGGER_UPGRADE_FLAGS_NONE);
 	ret = gs_plugin_loader_job_action (plugin_loader, plugin_job, NULL, &error);
 	gs_test_flush_main_context ();
 	g_assert_no_error (error);
