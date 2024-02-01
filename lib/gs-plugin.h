c56acbae47a81c8f666ddebfd8131d43a2d137d0
Milan Crha <mcrha@redhat.com>
2024-02-01 13:23:38 +0100

    gs-plugin: Add virtual functions for launch operation
    Helps: #1472

--- lib/gs-plugin.h
+++ lib/gs-plugin.h
@@ -113,6 +113,10 @@ G_DECLARE_DERIVABLE_TYPE (GsPlugin, gs_plugin, GS, PLUGIN, GObject)
  * @trigger_upgrade_finish: (nullable): Finish method for
  *   @trigger_upgrade_async. Must be implemented if
  *   @trigger_upgrade_async is implemented. (Since: 47)
+ * @launch_async: (nullable): Launch the specified app using a plugin-specific method. (Since: 47)
+ * @launch_finish: (nullable): Finish method for
+ *   @launch_async. Must be implemented if
+ *   @launch_async is implemented. (Since: 47)
  *
  * The class structure for a #GsPlugin. Virtual methods here should be
  * implemented by plugin implementations derived from #GsPlugin to provide their
@@ -313,6 +317,16 @@ struct _GsPluginClass
 								 GAsyncReadyCallback		 callback,
 								 gpointer			 user_data);
 	gboolean		(*trigger_upgrade_finish)	(GsPlugin			*plugin,
+								 GAsyncResult			*result,
+								 GError				**error);
+
+	void			(*launch_async)			(GsPlugin			*plugin,
+								 GsApp				*app,
+								 GsPluginLaunchFlags		 flags,
+								 GCancellable			*cancellable,
+								 GAsyncReadyCallback		 callback,
+								 gpointer			 user_data);
+	gboolean		(*launch_finish)		(GsPlugin			*plugin,
 								 GAsyncResult			*result,
 								 GError				**error);
 
