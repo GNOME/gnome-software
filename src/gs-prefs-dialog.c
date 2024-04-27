a26eb9ea716d6ab68dba026c9997396b3fdb7b93
Automeris naranja <automerisnaranja@tutanota.com>
2024-04-26 22:22:15 -0300

    prefs-dialog: Port to AdwSwitchRow

--- src/gs-prefs-dialog.c
+++ src/gs-prefs-dialog.c
@@ -25,9 +25,6 @@ struct _GsPrefsDialog
 	GsPluginLoader		*plugin_loader;
 	GtkWidget		*automatic_updates_radio;
 	GtkWidget		*manual_updates_radio;
-	GtkWidget		*switch_updates_notify;
-	GtkWidget		*switch_free_apps;
-	GtkWidget		*switch_verified_apps;
 	GtkPopover		*updates_info_popover;
 	AdwActionRow		*automatic_updates_row;
 	AdwActionRow		*manual_updates_row;
@@ -77,7 +74,7 @@ gs_prefs_dialog_init (GsPrefsDialog *dialog)
 	dialog->settings = g_settings_new ("org.gnome.software");
 	g_settings_bind (dialog->settings,
 			 "download-updates-notify",
-			 dialog->switch_updates_notify,
+			 dialog->automatic_update_notifications_row,
 			 "active",
 			 G_SETTINGS_BIND_DEFAULT);
 	g_settings_bind_with_mapping (dialog->settings,
@@ -89,17 +86,17 @@ gs_prefs_dialog_init (GsPrefsDialog *dialog)
 				      NULL, dialog, NULL);
 	g_settings_bind (dialog->settings,
 			 "show-only-free-apps",
-			 dialog->switch_free_apps,
+			 dialog->show_only_free_apps_row,
 			 "active",
 			 G_SETTINGS_BIND_DEFAULT);
 	g_settings_bind (dialog->settings,
 			 "show-only-verified-apps",
-			 dialog->switch_verified_apps,
+			 dialog->show_only_verified_apps_row,
 			 "active",
 			 G_SETTINGS_BIND_DEFAULT);
-	g_signal_connect_object (dialog->switch_free_apps, "notify::active",
+	g_signal_connect_object (dialog->show_only_free_apps_row, "notify::active",
 				 G_CALLBACK (gs_prefs_dialog_filters_changed_cb), dialog, G_CONNECT_SWAPPED);
-	g_signal_connect_object (dialog->switch_verified_apps, "notify::active",
+	g_signal_connect_object (dialog->show_only_verified_apps_row, "notify::active",
 				 G_CALLBACK (gs_prefs_dialog_filters_changed_cb), dialog, G_CONNECT_SWAPPED);
 
 #if ADW_CHECK_VERSION(1,2,0)
@@ -122,9 +119,6 @@ gs_prefs_dialog_class_init (GsPrefsDialogClass *klass)
 	gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Software/gs-prefs-dialog.ui");
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, automatic_updates_radio);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, manual_updates_radio);
-	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, switch_updates_notify);
-	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, switch_free_apps);
-	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, switch_verified_apps);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, updates_info_popover);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, automatic_updates_row);
 	gtk_widget_class_bind_template_child (widget_class, GsPrefsDialog, manual_updates_row);
