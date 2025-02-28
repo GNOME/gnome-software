GNOME/gnome-software

commit 0572bc6fcf080a4e5ac7650c6ca2e4158c3754e1

gs-shell: Always show a download error in the fallback error handler
The user should know the download failed, indicated in the GUI, not
only claim a warning on the terminal, which is often only in the journal
or lost entirely.

src/gs-shell.c
@@ -1910,6 +1910,10 @@ gs_shell_show_event_fallback (GsShell *shell, GsPluginEvent *event)
 							  str_origin);
 			toast_text = tmp_toast_text;
 			suggested_details_text = error->message;
+		} else {
+			/* TRANSLATORS: failure text for the in-app notification */
+			toast_text = _("Download failed");
+			suggested_details_text = error->message;
 		}
 	} else if (g_error_matches (error, GS_PLUGIN_ERROR, GS_PLUGIN_ERROR_NO_SPACE)) {
 		/* TRANSLATORS: failure text for the in-app notification */

commit 3cad018ad0f19474a947da3e06a459a45c8b32cc

gs-shell: Fix a string freeze break by re-using an existing string
We’re well into string freeze for GNOME 48, and commit
0572bc6fcf080a4e5ac7650c6ca2e4158c3754e1 accidentally introduced a new
translatable string.

Change it to a relatively appropriate already-existing string for GNOME
48, and we can improve on this for GNOME 49 once string freeze has
thawed.

I can’t work out how to get this string to appear in the UI anyway, so
hopefully this has low impact.

Signed-off-by: Philip Withnall <pwithnall@gnome.org>

Fixes: https://gitlab.gnome.org/Teams/Translation/Coordination/-/issues/258

src/gs-shell.c
@@ -1916,7 +1916,7 @@ gs_shell_show_event_fallback (GsShell *shell, GsPluginEvent *event)
 			suggested_details_text = error->message;
 		} else {
 			/* TRANSLATORS: failure text for the in-app notification */
-			toast_text = _("Download failed");
+			toast_text = _("Sorry, something went wrong");
 			suggested_details_text = error->message;
 		}
 	} else if (g_error_matches (error, GS_PLUGIN_ERROR, GS_PLUGIN_ERROR_NO_SPACE)) {